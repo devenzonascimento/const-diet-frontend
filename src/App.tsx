6b142cbfee385ecb81cd352dd3cc72b02ccd5544
devenzonascimento <dev.enzody@gmail.com>
2024-05-02 19:46:22 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AddNewFoodManuallyPage from "./pages/AddNewFoodManually/page";
+//import MyFoodsPage from "./pages/MyFoods/page";
+//import LoginPage from "./pages/Login/page";
+
+function App() {
+  return (
+    <>
+      <AddNewFoodManuallyPage />
+    </>
+  );
+}
+
+export default App;

2e08801aba2c2eb228efd1dcdbb9739045c43388
devenzonascimento <dev.enzody@gmail.com>
2024-05-03 21:04:51 -0300

    add Poppins font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      "body": "Poppins, Arial, Helvetica, sans-serif"
+    },
     container: {
       center: true,
       padding: "2rem",

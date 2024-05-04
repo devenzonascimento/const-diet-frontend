ff3559367628c6d26f0ad4e9407581f6057e99e0
devenzonascimento <dev.enzody@gmail.com>
2024-05-03 21:18:15 -0300

    feat: add link to add new food manually page, and update some styles

--- src/pages/MyFoods/page.tsx
+++ src/pages/MyFoods/page.tsx
@@ -2,15 +2,19 @@ import InputSearch from "@/components/input-search";
 import { ArrowLeft, CirclePlus } from "lucide-react";
 import FoodsList from "./components/foods-list";
 
+import { Link } from "react-router-dom";
+
 const MyFoodsPage = () => {
   return (
-    <div className="bg-gray-200">
-      <header className="flex justify-between items-center p-4">
-        <ArrowLeft size={32} onClick={() => (console.log("voltar"))} />
+    <div className="h-screen bg-slate-100 px-4">
+      <header className="relative flex justify-center items-center py-4">
+        <ArrowLeft size={32} className="absolute top-4 left-0"/>
         <h1 className="text-xl font-semibold">Meus alimentos</h1>
-        <CirclePlus size={32} />
+        <Link to="/add-new-food-manually">
+          <CirclePlus size={32} className="absolute top-4 right-0"/>
+        </Link>
       </header>
-      <main className="flex flex-col justify-center items-center px-4">
+      <main className="flex flex-col justify-center items-center gap-8 pb-6">
         <InputSearch />
         <FoodsList />
       </main>

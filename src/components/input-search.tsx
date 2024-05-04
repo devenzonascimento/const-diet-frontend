18aff14ada24470108055a91cee66ba31c5b924e
devenzonascimento <dev.enzody@gmail.com>
2024-05-03 21:09:19 -0300

    style: improve styles and add hover effect

--- src/components/input-search.tsx
+++ src/components/input-search.tsx
@@ -1,12 +1,14 @@
 import { Search } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 const InputSearch = () => {
   return (
-    <fieldset className="w-full flex justify-between items-center gap-2 bg-white rounded-3xl overflow-hidden">
-      <input className="w-full py-1 pl-3 bg-transparent" type="text" placeholder="Buscar alimento" />
-      <button className="flex justify-center items-center bg-sky-700 p-2 rounded-full">
+    <fieldset className="w-full flex justify-between items-center gap-2 bg-white rounded-3xl shadow-lg overflow-hidden">
+      <Input className="w-full py-1 pl-3 text-md text-body bg-transparent" type="text" placeholder="Buscar alimento"/>
+      <Button className="flex justify-center items-center bg-sky-700 p-2 rounded-full hover:bg-sky-500">
         <Search className="text-white" size={24}/>
-      </button>
+      </Button>
     </fieldset>
   );
 };

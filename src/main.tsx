64e6e64329c468ceb71cf136c404dc08ccc3b58f
devenzonascimento <dev.enzody@gmail.com>
2024-05-03 21:06:08 -0300

    feat: add routes for all pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+//import App from './App.tsx'
 
 import "../app/globals.css"
 
+
+import AddNewFoodManuallyPage from "./pages/AddNewFoodManually/page";
+import MyFoodsPage from "./pages/MyFoods/page";
+import LoginPage from "./pages/Login/page";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <LoginPage />
+  },
+  {
+    path: "my-foods",
+    element: <MyFoodsPage />
+  },
+  {
+    path: "add-new-food-manually",
+    element: <AddNewFoodManuallyPage />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )

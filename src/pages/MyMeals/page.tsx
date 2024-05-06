a42d97ac58e6f444aa5821e0cea2d895bf67a1b1
devenzonascimento <dev.enzody@gmail.com>
2024-05-06 16:32:50 -0300

    feat: create meals page

---
+++ src/pages/MyMeals/page.tsx
@@ -0,0 +1,22 @@
+import { ArrowLeft } from "lucide-react";
+
+import ProgressBar from "./components/progress-bar";
+import MealsList from "./components/meals-list";
+
+const MyMealsPage = () => {
+  return (
+    <div className="h-screen bg-slate-100 px-4">
+      <header className="relative flex justify-center items-center py-4">
+        <ArrowLeft size={32} className="absolute top-4 left-0" />
+        <h1 className="text-xl font-semibold">Plano de hoje</h1>
+      </header>
+      <main className="flex flex-col justify-between items-center gap-4">
+        <ProgressBar />
+
+        <MealsList />
+      </main>
+    </div>
+  );
+};
+
+export default MyMealsPage;

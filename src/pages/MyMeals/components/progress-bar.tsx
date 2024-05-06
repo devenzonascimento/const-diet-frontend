ce059f1c20a8911d3f9ee6ebfcdb836302b64ae3
devenzonascimento <dev.enzody@gmail.com>
2024-05-06 16:32:07 -0300

    feat: add progress bar component to My Meals page

---
+++ src/pages/MyMeals/components/progress-bar.tsx
@@ -0,0 +1,16 @@
+import { Progress } from "@/components/ui/progress";
+
+const ProgressBar = () => {
+  return (
+    <div className=" h-20 w-full flex flex-col justify-center items-center gap-1 px-6 bg-white border-4 border-sky-700 rounded-xl">
+      <p className="font-semibold text-xl text-sky-600">Refeições concluídas</p>
+      <div className="w-full flex justify-between items-center gap-4">
+        <p className="font-semibold text-lg text-sky-600">2</p>
+        <Progress value={40} className="bg-gray-300" />
+        <p className="font-semibold text-lg text-sky-600">5</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;

91fe0581acee2382fdfa993ada694f6935cf98bc
devenzonascimento <dev.enzody@gmail.com>
2024-05-06 19:07:04 -0300

    style: change colors palete

--- src/pages/MyFoods/components/macronutrient-badges.tsx
+++ src/pages/MyFoods/components/macronutrient-badges.tsx
@@ -10,19 +10,19 @@ const MacronutrientsBadge = ({ nutrients }: MacronutrientsBadgeProps) => {
   const { carbo, protein, fat } = nutrients;
 
   return (
-    <div className="w-full flex justify-center gap-3">
+    <div className="w-fit flex justify-center gap-3">
       <p
-        className={`px-1 block font-medium text-blue-600 bg-blue-100 rounded-lg`}
+        className={`px-1 block font-medium text-blue-700 bg-blue-200 rounded-lg`}
       >
         {`C: ${carbo}g`}
       </p>
       <p
-        className={`px-1 block font-medium text-red-600 bg-red-100 rounded-lg`}
+        className={`px-1 block font-medium text-cyan-700 bg-cyan-200 rounded-lg`}
       >
         {`P: ${protein}g`}
       </p>
       <p
-        className={`px-1 block font-medium text-orange-600 bg-orange-100 rounded-lg`}
+        className={`px-1 block font-medium text-sky-700 bg-sky-200 rounded-lg`}
       >
         {`G: ${fat}g`}
       </p>

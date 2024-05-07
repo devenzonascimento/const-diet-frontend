1b7fbabfd8508339450afa6dceba64cf3929b416
devenzonascimento <dev.enzody@gmail.com>
2024-05-07 11:28:00 -0300

    style: add border to input search, e clean up some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import AddNewFoodManuallyPage from "./pages/AddNewFoodManually/page";
-//import MyFoodsPage from "./pages/MyFoods/page";
-//import LoginPage from "./pages/Login/page";
+import MyFoodsPage from "./pages/MyFoods/page";
+import LoginPage from "./pages/Login/page";
 
 function App() {
   return (
     <>
       <AddNewFoodManuallyPage />
+      <MyFoodsPage />
+      <LoginPage />
     </>
   );
 }
--- src/components/input-search.tsx
+++ src/components/input-search.tsx
@@ -4,9 +4,9 @@ import { Input } from "./ui/input";
 
 const InputSearch = () => {
   return (
-    <fieldset className="w-full flex justify-between items-center gap-2 bg-white rounded-3xl shadow-lg overflow-hidden">
-      <Input className="w-full py-1 pl-3 text-md text-body bg-transparent" type="text" placeholder="Buscar alimento"/>
-      <Button className="flex justify-center items-center bg-sky-700 p-2 rounded-full hover:bg-sky-500">
+    <fieldset className="relative w-full shadow-lg">
+      <Input className="w-full py-1 px-3 pr-12 text-md bg-white rounded-3xl border-2 border-sky-700" type="text" placeholder="Buscar alimento"/>
+      <Button className=" absolute top-0 right-0 bg-sky-700 p-2 rounded-full hover:bg-sky-500">
         <Search className="text-white" size={24}/>
       </Button>
     </fieldset>

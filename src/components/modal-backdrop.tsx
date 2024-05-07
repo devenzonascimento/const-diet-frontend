e10992a25aeb260240eb58a5741eace3fa89f41b
devenzonascimento <dev.enzody@gmail.com>
2024-05-07 14:08:53 -0300

    feat: add modal backdrop component

---
+++ src/components/modal-backdrop.tsx
@@ -0,0 +1,22 @@
+import useBackdropClose from "@/hooks/useBackdropClose";
+import { ReactNode } from "react";
+
+interface ModalBackdropProps {
+  onClose: VoidFunction;
+  children: ReactNode;
+}
+
+const ModalBackdrop = ({ children, onClose }: ModalBackdropProps) => {
+  const backdropRef = useBackdropClose(onClose);
+
+  return (
+    <dialog
+      className="fixed top-0 z-10 h-screen w-screen flex justify-center items-center bg-black/70"
+      ref={backdropRef}
+    >
+      {children}
+    </dialog>
+  );
+};
+
+export default ModalBackdrop;

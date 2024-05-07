edd919f47e8fa405a729cfdcfc5474667b76d021
devenzonascimento <dev.enzody@gmail.com>
2024-05-07 15:50:35 -0300

    refactor: move folders to a new folder called Foods

--- src/pages/Foods/MyFoods/components/food-card-modal.tsx
+++ src/pages/Foods/MyFoods/components/food-card-modal.tsx
@@ -2,6 +2,7 @@ import CaloriesBadge from "@/components/calories-badge";
 import MacronutrientsBadge from "@/components/macronutrient-badges";
 import ModalBackdrop from "@/components/modal-backdrop";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 interface FoodCardModalProps {
   food: Food;
@@ -10,8 +11,16 @@ interface FoodCardModalProps {
 
 const FoodCardModal = ({
   onClose,
-  food: { name, image, calories, nutrients },
+  food: { id, name, image, calories, nutrients },
 }: FoodCardModalProps) => {
+
+    const navigate = useNavigate();
+
+    const test = () => {
+        console.log('chamou')
+        navigate(`/edit-food/${id}`)
+    }
+
   return (
     <ModalBackdrop onClose={onClose}>
       <div className="w-full h-fit mx-6 flex flex-col justify-between items-center gap-6 p-8 bg-slate-100 border-4 border-sky-500 rounded-lg shadow-xl">
@@ -23,7 +32,8 @@ const FoodCardModal = ({
         <h1 className="text-2xl font-semibold text-center ">{name}</h1>
         <CaloriesBadge className="scale-150" calories={calories} />
         <MacronutrientsBadge className="scale-110" nutrients={nutrients} />
-        <Button type="submit" className="w-full bg-sky-700 hover:bg-sky-500">
+        <Button type="submit" className="w-full bg-sky-700 hover:bg-sky-500"
+        onClick={test}>
           Editar alimento
         </Button >
       </div>

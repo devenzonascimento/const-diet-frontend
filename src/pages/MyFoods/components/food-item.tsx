b143db538966212bf7a30de02997dba6915a3f24
devenzonascimento <dev.enzody@gmail.com>
2024-05-07 14:41:21 -0300

    feat: integrate food card, called for when component, and abstract calories badge to a separete component

--- src/pages/MyFoods/components/food-item.tsx
+++ src/pages/MyFoods/components/food-item.tsx
@@ -1,6 +1,8 @@
 import MacronutrientsBadge from "@/components/macronutrient-badges";
-import { Flame } from "lucide-react";
-
+import { useState } from "react";
+import FoodCardModal from "./food-card-modal";
+import When from "@/components/when";
+import CaloriesBadge from "@/components/calories-badge";
 
 interface FoodItemProps {
   food: Food;
@@ -9,25 +11,39 @@ interface FoodItemProps {
 const FoodItem = ({ food }: FoodItemProps) => {
   const { image, name, calories, nutrients } = food;
 
-  return (
-    <li className="relative w-full flex gap-1 bg-white rounded-lg shadow-lg border-4 border-sky-700">
-      <div className="h-20 aspect-square flex items-start border-r-4 border-sky-700 ">
-        <img
-          className="h-full  aspect-square object-cover rounded-l-md shadow-xl"
-          src={image}
-          alt="food"
-        />
-      </div>
-      <div className="w-full flex flex-col justify-between p-2">
-        <h2 className="font-semibold text-gray-800">{name}</h2>
-        <MacronutrientsBadge nutrients={nutrients} />
-      </div>
+  const [isFoodCardOpen, setIsFoodCardOpen] = useState(false);
 
-      <div className="absolute top-1 right-2 flex items-center gap-1 text-gray-800 font-semibold">
-        <Flame className="text-red-600 fill-yellow-500" size={20} />
-        <p className="pt-1">{calories}</p>
-      </div>
-    </li>
+  const handleToggleCard = () => {
+    setIsFoodCardOpen(!isFoodCardOpen);
+
+    return;
+  };
+
+  return (
+    <>
+      <li
+        className="relative w-full flex gap-1 bg-white rounded-lg shadow-lg border-4 border-sky-700"
+        onClick={handleToggleCard}
+      >
+        <div className="h-20 aspect-square flex items-start border-r-4 border-sky-700 ">
+          <img
+            className="h-full  aspect-square object-cover rounded-l-md shadow-xl"
+            src={image}
+            alt="food"
+          />
+        </div>
+        <div className="w-full flex flex-col justify-between p-2">
+          <h2 className="font-semibold text-gray-800">{name}</h2>
+          <MacronutrientsBadge nutrients={nutrients} />
+        </div>
+        
+        <CaloriesBadge className="absolute top-0 right-1" calories={calories} />
+   
+      </li>
+      <When expr={isFoodCardOpen}>
+        <FoodCardModal food={food} onClose={handleToggleCard}/>
+      </When>
+    </>
   );
 };
 

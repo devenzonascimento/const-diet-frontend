6c600dcc5150f70597f6c4736b0774a8749cb183
devenzonascimento <dev.enzody@gmail.com>
2024-05-07 15:24:03 -0300

    refactor: use a modal hook for manipulate modal state

--- src/pages/MyFoods/components/food-item.tsx
+++ src/pages/MyFoods/components/food-item.tsx
@@ -1,8 +1,8 @@
 import MacronutrientsBadge from "@/components/macronutrient-badges";
-import { useState } from "react";
 import FoodCardModal from "./food-card-modal";
 import When from "@/components/when";
 import CaloriesBadge from "@/components/calories-badge";
+import useModalSate from "@/hooks/useModalState";
 
 interface FoodItemProps {
   food: Food;
@@ -11,19 +11,13 @@ interface FoodItemProps {
 const FoodItem = ({ food }: FoodItemProps) => {
   const { image, name, calories, nutrients } = food;
 
-  const [isFoodCardOpen, setIsFoodCardOpen] = useState(false);
-
-  const handleToggleCard = () => {
-    setIsFoodCardOpen(!isFoodCardOpen);
-
-    return;
-  };
+  const {isOpen, toggleModal} = useModalSate()
 
   return (
     <>
       <li
         className="relative w-full flex gap-1 bg-white rounded-lg shadow-lg border-4 border-sky-700"
-        onClick={handleToggleCard}
+        onClick={toggleModal}
       >
         <div className="h-20 aspect-square flex items-start border-r-4 border-sky-700 ">
           <img
@@ -40,8 +34,8 @@ const FoodItem = ({ food }: FoodItemProps) => {
         <CaloriesBadge className="absolute top-0 right-1" calories={calories} />
    
       </li>
-      <When expr={isFoodCardOpen}>
-        <FoodCardModal food={food} onClose={handleToggleCard}/>
+      <When expr={isOpen}>
+        <FoodCardModal food={food} onClose={toggleModal}/>
       </When>
     </>
   );

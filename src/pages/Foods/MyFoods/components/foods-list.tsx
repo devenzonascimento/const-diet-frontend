2abe86fd67d69f02aa0e36b91d1954ca17732412
devenzonascimento <dev.enzody@gmail.com>
2024-05-08 12:43:58 -0300

    refactor: remove logic into component and move to a custom hook

--- src/pages/Foods/MyFoods/components/foods-list.tsx
+++ src/pages/Foods/MyFoods/components/foods-list.tsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from "react";
 import FoodItem from "./food-item";
+import useFetchFoodData from "@/hooks/useFetchFoodData";
 
 const FoodsList = () => {
-
-  const [foodsList, setFoodsList] = useState<Food[]>([])
-  
-  useEffect(() => {
-    const fetchData = async () => {
-
-      const url = "src/data/foods.json"
-
-      const response = await fetch(url)
-      const responseJSON = await response.json()
-      setFoodsList(responseJSON)
-    }
-
-    fetchData()
-    
-  }, [])
+  const { foodsList } = useFetchFoodData();
 
   return (
     <ul className="w-full flex flex-col gap-6">
-        {foodsList.map((food) => (
-            <FoodItem key={food.id} food={food}/>
-        ))}
+      {foodsList.map((food) => (
+        <FoodItem key={food.id} food={food} />
+      ))}
     </ul>
   );
 };

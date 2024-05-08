f94db082979694823a3b151e14275ad672f69f7d
devenzonascimento <dev.enzody@gmail.com>
2024-05-08 10:05:26 -0300

    style: add a "kcal" text aside calories number

--- src/components/calories-badge.tsx
+++ src/components/calories-badge.tsx
@@ -8,9 +8,12 @@ interface CaloriesBadgeProps {
 
 const CaloriesBadge = ({ className, calories }: CaloriesBadgeProps) => {
   return (
-    <div className={cn("flex items-center gap-1", className)}>
+    <div className={cn("flex items-center gap-0.5", className)}>
       <Flame className="text-red-600 fill-yellow-500" size={20} />
-      <p className="pt-1 text-gray-800 font-semibold">{calories}</p>
+      <p className="pt-1 text-gray-800 font-semibold">
+        {calories}
+        <span className="text-xs">kcal</span>
+      </p>
     </div>
   );
 };

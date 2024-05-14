0a4288f2ce299ce8d48ab72cb83356a02530c812
devenzonascimento <dev.enzody@gmail.com>
2024-05-14 10:53:27 -0300

    refactor: add a open and check logicals, and abstract peaces to a separete components

--- src/pages/MyMeals/components/meal-item.tsx
+++ src/pages/MyMeals/components/meal-item.tsx
@@ -1,8 +1,9 @@
-import MacronutrientsBadge from "@/components/macronutrient-badges";
-import { Button } from "@/components/ui/button";
-import useFetchFoodData from "@/hooks/useFetchFoodData";
+import useToggleState from "@/hooks/useToggleState";
+
+import MealDescription from "./meal-description";
+import ChevronButton from "./chevron-button";
+
 import { Meal } from "@/types/types";
-import { ChevronDown } from "lucide-react";
 
 interface MealItemProps {
   meal: Meal;
@@ -11,12 +12,14 @@ interface MealItemProps {
 const MealItem = ({ meal }: MealItemProps) => {
   const { name, foods, time } = meal;
 
+  const { booleanExp, toggleBooleanExp } = useToggleState();
+
   return (
-    <div className="w-full flex flex-col px-2 bg-white rounded-2xl shadow-xl border-4 border-sky-700">
-      <div className=" w-full flex justify-between items-center">
-        <Button variant={"ghost"} className="p-0">
-          <ChevronDown className="text-sky-700" />
-        </Button>
+    <li
+      className="w-full flex flex-col gap-2 bg-white rounded-xl shadow-xl border-4 border-sky-700"
+    >
+      <div className=" w-full flex justify-between items-center p-2" onClick={toggleBooleanExp}>
+        <ChevronButton isOpen={booleanExp} />
         <h2 className=" w-fit text-xl uppercase font-semibold text-sky-700 text-center">
           {name}
         </h2>
@@ -24,44 +27,9 @@ const MealItem = ({ meal }: MealItemProps) => {
           {time.hour}:{time.minute}
         </p>
       </div>
-
-    </div>
-  );
-};
-//TODO: fazer pique que um faq para clicar e abrir a lista de alimentos, e ao mesmo tempo ser uma todo list
-export default MealItem;
-
-interface DescriptionProps {
-  foods: DescriptionItemProps[];
-}
-
-const Description = ({ foods }: DescriptionProps) => {
-  return (
-    <ul className="flex flex-col gap-2 px-4">
-      {foods.map((food) => {
-        return (
-          <DescriptionItem
-            key={food.id}
-            id={food.id}
-            portion={food.portion}
-          />
-        );
-      })}
-    </ul>
-  );
-};
-
-interface DescriptionItemProps {
-  id: string;
-  portion: number;
-}
-
-const DescriptionItem = ({ id, portion }: DescriptionItemProps) => {
-  const { food } = useFetchFoodData(id);
-
-  return (
-    <li className="w-full p-1">
-      <p className="text-sky-700">{`${portion} gramas de ${food?.name}`}</p>
+      <MealDescription foods={foods} isOpen={booleanExp} />
     </li>
   );
 };
+
+export default MealItem;

143dc918ad27922ec24f517cb6adeb2b0e30589c
devenzonascimento <dev.enzody@gmail.com>
2024-05-14 10:37:39 -0300

    style: fix a bug in field's border

--- src/components/input-search.tsx
+++ src/components/input-search.tsx
@@ -4,7 +4,7 @@ import { Input } from "./ui/input";
 
 const InputSearch = () => {
   return (
-    <fieldset className="relative w-full shadow-lg">
+    <fieldset className="relative w-full shadow-lg bg-transparent rounded-3xl">
       <Input className="w-full py-1 px-3 pr-12 text-md bg-white rounded-3xl border-2 border-sky-700" type="text" placeholder="Buscar alimento"/>
       <Button className=" absolute top-0 right-0 bg-sky-700 p-2 rounded-full hover:bg-sky-500">
         <Search className="text-white" size={24}/>

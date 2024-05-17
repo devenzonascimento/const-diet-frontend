85e92f87ee4afe9196b34d1918577eda67c0cd18
devenzonascimento <dev.enzody@gmail.com>
2024-05-17 11:33:36 -0300

    fix: remove temporally image property, and import Food Interface

--- src/pages/Foods/MyFoods/components/food-item.tsx
+++ src/pages/Foods/MyFoods/components/food-item.tsx
@@ -3,16 +3,31 @@ import FoodCardModal from "./food-card-modal";
 import When from "@/components/when";
 import CaloriesBadge from "@/components/calories-badge";
 import useModalSate from "@/hooks/useModalState";
+import { Food } from "@/types/types";
 
 interface FoodItemProps {
   food: Food;
 }
 
 const FoodItem = ({ food }: FoodItemProps) => {
-  const { image, name, calories, nutrients } = food;
+  const { name, calories, nutrients } = food;
 
   const {isOpen, toggleModal} = useModalSate()
-
+/*
+{
+    "userId": "f24e3e04-59a1-40e2-8b11-2854b017f15b",
+    "id": "6777f582-8462-42d8-b358-bfff68d50e05",
+    "name": "Arroz",
+    "calories": 130,
+    "nutrients": {
+      "carbohydrates": 25,
+      "proteins": 2,
+      "fats": 0.5,
+      "sodiums": 0,
+      "fibers": 1,
+    }
+}
+*/
   return (
     <>
       <li
@@ -22,7 +37,7 @@ const FoodItem = ({ food }: FoodItemProps) => {
         <div className="h-20 aspect-square flex items-start border-r-4 border-sky-700 ">
           <img
             className="h-full  aspect-square object-cover rounded-l-md shadow-xl"
-            src={image}
+            //src={image}
             alt="food"
           />
         </div>

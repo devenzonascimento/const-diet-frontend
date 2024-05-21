e29699f6752c97892295225045b30c4fc91f5aa9
devenzonascimento <dev.enzody@gmail.com>
2024-05-21 15:41:06 -0300

    refactor: add a default value for food ID parameter

--- src/hooks/useFetchFood.ts
+++ src/hooks/useFetchFood.ts
@@ -2,7 +2,7 @@ import { api } from "@/services/api";
 import { Food } from "@/types/types";
 import { useEffect, useState } from "react";
 
-export const useFetchFood = (foodId: string) => {
+export const useFetchFood = (foodId: string = "") => {
   const [food, setFood] = useState<Food>();
 
   const fetchFood = async () => {

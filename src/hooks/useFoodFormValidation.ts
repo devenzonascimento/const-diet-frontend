7bb5b6e77e4bb4543e1308a6a5bbc69463bfe26c
devenzonascimento <dev.enzody@gmail.com>
2024-05-21 15:39:52 -0300

    refactor: abstracts hook form and zod logics to a separate component

---
+++ src/hooks/useFoodFormValidation.ts
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { Food } from "@/types/types"
+
+const foodFormSchema = z.object({
+  name: z.string().min(3),
+  calories: z.coerce.number(),
+  carbohydrates: z.coerce.number(),
+  proteins: z.coerce.number(),
+  fats: z.coerce.number(),
+  sodiums: z.coerce.number(),
+  fibers: z.coerce.number(),
+})
+
+type FoodFormSchema = z.infer<typeof foodFormSchema>
+
+export const useFoodFormValidation = (food?: Food) => {
+
+  
+  const { register, handleSubmit, setValue } = useForm<FoodFormSchema>({
+    resolver: zodResolver(foodFormSchema),
+    defaultValues: food
+  });
+
+  useEffect(() => {
+    if (food) {
+      Object.entries(food).forEach(([key, value]) => {
+        setValue(key as keyof FoodFormSchema, value);
+      });
+    }
+  }, [food, setValue]);
+
+  return {
+    register,
+    handleSubmit,
+  }
+}
--- src/pages/Foods/EditFoodPage/page.tsx
+++ src/pages/Foods/EditFoodPage/page.tsx
@@ -1,47 +1,18 @@
-import { useEffect } from "react";
-
 import { Link, useParams } from "react-router-dom";
-
 import { useFetchFood } from "@/hooks/useFetchFood";
 import { updateFood } from "@/services/http/food/update-food";
-
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ArrowLeft, Camera } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import InputDefault from "@/components/input-default";
-
-const foodFormSchema = z.object({
-  name: z.string().min(3),
-  calories: z.coerce.number(),
-  carbohydrates: z.coerce.number(),
-  proteins: z.coerce.number(),
-  fats: z.coerce.number(),
-  sodiums: z.coerce.number(),
-  fibers: z.coerce.number(),
-})
-
-type FoodFormSchema = z.infer<typeof foodFormSchema>
+import { useFoodFormValidation } from "@/hooks/useFoodFormValidation";
 
 const EditFoodPage = () => {
   const { id } = useParams<string>();
 
   const { food } = useFetchFood(id)
 
-  const { register, handleSubmit, setValue } = useForm<FoodFormSchema>({
-    resolver: zodResolver(foodFormSchema),
-    defaultValues: food
-  });
-
-  useEffect(() => {
-    if (food) {
-      Object.entries(food).forEach(([key, value]) => {
-        setValue(key as keyof FoodFormSchema, value);
-      });
-    }
-  }, [food, setValue]);
+  const { register, handleSubmit } = useFoodFormValidation(food)
 
   return (
     <div className="h-screen bg-gray-00 px-4">

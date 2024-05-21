e32e20ca037337316f23b8aaaff03bc8c0b18382
devenzonascimento <dev.enzody@gmail.com>
2024-05-21 15:30:54 -0300

    feat: add a update food feature initial

--- src/pages/Foods/EditFoodPage/page.tsx
+++ src/pages/Foods/EditFoodPage/page.tsx
@@ -1,15 +1,47 @@
-import { useFetchFood } from "@/hooks/useFetchFood";
+import { useEffect } from "react";
 
 import { Link, useParams } from "react-router-dom";
 
-import FoodForm from "../../../components/food-form";
+import { useFetchFood } from "@/hooks/useFetchFood";
+import { updateFood } from "@/services/http/food/update-food";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ArrowLeft, Camera } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import InputDefault from "@/components/input-default";
+
+const foodFormSchema = z.object({
+  name: z.string().min(3),
+  calories: z.coerce.number(),
+  carbohydrates: z.coerce.number(),
+  proteins: z.coerce.number(),
+  fats: z.coerce.number(),
+  sodiums: z.coerce.number(),
+  fibers: z.coerce.number(),
+})
+
+type FoodFormSchema = z.infer<typeof foodFormSchema>
 
 const EditFoodPage = () => {
-  const { id } = useParams();
+  const { id } = useParams<string>();
 
-  const { food } = useFetchFood(id ?? "")
+  const { food } = useFetchFood(id)
+
+  const { register, handleSubmit, setValue } = useForm<FoodFormSchema>({
+    resolver: zodResolver(foodFormSchema),
+    defaultValues: food
+  });
+
+  useEffect(() => {
+    if (food) {
+      Object.entries(food).forEach(([key, value]) => {
+        setValue(key as keyof FoodFormSchema, value);
+      });
+    }
+  }, [food, setValue]);
 
   return (
     <div className="h-screen bg-gray-00 px-4">
@@ -26,7 +58,50 @@ const EditFoodPage = () => {
         <div className="h-40 aspect-square flex justify-center items-center bg-black rounded-xl">
           <Camera className="text-white" size={64} />
         </div>
-        <FoodForm foodData={food}/>
+        <form
+          className="w-full flex flex-col gap-6 py-4"
+          onSubmit={handleSubmit((data) => updateFood(data, id))}
+        >
+          <InputDefault
+            id="foodName-input"
+            label="Nome do alimento"
+            {...register("name")}
+          />
+          <InputDefault
+            id="carbo-input"
+            label="Calorias"
+            {...register("calories")}
+          />
+          <InputDefault
+            id="carbo-input"
+            label="Carboidratos em gramas"
+            {...register("carbohydrates")}
+          />
+          <InputDefault
+            id="protein-input"
+            label="Proteínas em gramas"
+            {...register("proteins")}
+          />
+          <InputDefault
+            id="fat-input"
+            label="Gorduras em gramas"
+            {...register("fats")}
+          />
+          <InputDefault
+            id="sodium-input"
+            label="Sódios em gramas"
+            {...register("sodiums")}
+          />
+          <InputDefault
+            id="fiber-input"
+            label="Fibra em gramas"
+            {...register("fibers")}
+          />
+
+          <Button type="submit" className="w-full bg-sky-700 hover:bg-sky-500">
+            Salvar alimento
+          </Button>
+        </form>
       </main>
     </div>
   );

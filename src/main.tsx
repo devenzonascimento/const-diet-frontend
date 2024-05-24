2f446f2700a0e04e8022d674e600a9c5f4ca6a3a
devenzonascimento <dev.enzody@gmail.com>
2024-05-24 14:17:38 -0300

    chore: add react query pack

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
 import "../app/globals.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )

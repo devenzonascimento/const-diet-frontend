05c074e38078b753009374cd1453beb90df7941b
devenzonascimento <dev.enzody@gmail.com>
2024-05-27 12:27:33 -0300

    feat: add a macronutrients badge component

--- src/pages/Meals/components/meal-description.tsx
+++ src/pages/Meals/components/meal-description.tsx
@@ -1,3 +1,7 @@
+import { calculateTotalNutrients } from "@/functions/calculate-total-nutrients";
+
+import MacronutrientsBadge from "@/components/macronutrient-badges";
+
 import { MealFood } from "@/types/types";
 
 interface MealDescriptionProps {
@@ -14,13 +18,14 @@ export const MealDescription = ({ foods, isOpen }: MealDescriptionProps) => {
     >
       {foods?.map((foodItem) => {
         return (
-          <li key={foodItem.id} className="w-full p-1">
+          <li key={foodItem.id} className="w-full p-1 text-center">
             <p className="text-sky-700">
               {`${foodItem.quantity} gramas de ${foodItem.food.name}`}
             </p>
           </li>
         );
       })}
+      <MacronutrientsBadge className="m-auto pt-6" nutrients={calculateTotalNutrients(foods)} />
     </ul>
   );
 };

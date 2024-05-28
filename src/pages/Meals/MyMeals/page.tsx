7dadc0345e41f5e7d8048ca99de6c54b7fd8f429
devenzonascimento <dev.enzody@gmail.com>
2024-05-28 17:11:21 -0300

    feat: add a button to navigate to Add New Meal page

--- src/pages/Meals/MyMeals/page.tsx
+++ src/pages/Meals/MyMeals/page.tsx
@@ -1,13 +1,17 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, CirclePlus } from "lucide-react";
 
 import MealsList from "../components/meals-list";
+import { Link } from "react-router-dom";
 
-const MyMealsPage = () => {
+export const MyMealsPage = () => {
   return (
     <div className="h-screen bg-slate-100 px-4">
       <header className="relative flex justify-center items-center py-4">
         <ArrowLeft size={32} className="absolute top-4 left-0" />
         <h1 className="text-xl font-semibold">Minhas refeições</h1>
+        <Link to="/add-new-meal">
+          <CirclePlus size={32} className="absolute top-4 right-0" />
+        </Link>
       </header>
       <main className="flex flex-col justify-between items-center gap-4">
 
@@ -16,5 +20,3 @@ const MyMealsPage = () => {
     </div>
   );
 };
-
-export default MyMealsPage;

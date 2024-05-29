f5eee599423ec9fcddbf56c998309fcc8622e472
devenzonascimento <dev.enzody@gmail.com>
2024-05-29 14:12:47 -0300

    feat: add new meal context provider around the application

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,14 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/react-query'
 
 import "../app/globals.css"
+import { NewMealProvider } from './pages/Meals/AddNewMeal/context/new-meal-context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <NewMealProvider>
+        <App />
+      </NewMealProvider>
     </QueryClientProvider>
   </StrictMode>,
 )

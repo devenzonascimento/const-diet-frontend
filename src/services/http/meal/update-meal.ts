474278f232192b96f08c5940debed3eabf85082e
devenzonascimento <dev.enzody@gmail.com>
2024-05-30 16:26:02 -0300

    feat: add a http request function to update meal data

---
+++ src/services/http/meal/update-meal.ts
@@ -0,0 +1,20 @@
+import { api } from "@/services/api"
+
+interface UpdateMeal {
+  mealId: string;
+  mealName: string;
+  foods: {
+    foodId: string;
+    quantity: number;
+    unit: string;
+  }[];
+}
+
+export const updateMeal = async ({mealId, ...mealData}:  UpdateMeal) => {
+
+  const userId = localStorage.getItem("userId")
+
+  const response = await api.put(`/users/${userId}/meals/${mealId}`, mealData)
+
+  return response.status === 200
+}

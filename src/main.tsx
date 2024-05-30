08bf9fd4e2de7c768777934bb785e1af1eba0988
devenzonascimento <dev.enzody@gmail.com>
2024-05-30 13:36:12 -0300

    chore: organize setup files

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { queryClient } from './lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { NewMealProvider } from './pages/Meals/AddNewMeal/context/new-meal-context'
+
 import { App } from './App'
 
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/react-query'
-
-import "../app/globals.css"
-import { NewMealProvider } from './pages/Meals/AddNewMeal/context/new-meal-context'
+import "./globals.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>

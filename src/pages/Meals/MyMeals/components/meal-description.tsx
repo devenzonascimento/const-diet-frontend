8fd6cdb8506f08cdb3df2f6540ba8127a58d419d
devenzonascimento <dev.enzody@gmail.com>
2024-05-30 14:23:32 -0300

    refactor: abstracts part of component to a hard code in parent component

--- src/pages/Meals/MyMeals/components/meal-description.tsx
+++ src/pages/Meals/MyMeals/components/meal-description.tsx
@@ -1,15 +1,10 @@
 import { MealFood } from "@/types/types";
-import { Button } from "@/components/ui/button";
-import { DeleteButton } from "./delete-button";
-
 
 interface MealDescriptionProps {
   foods: MealFood[] | undefined
-  isOpen: boolean;
-  deleteMeal: VoidFunction
 }
 
-export const MealDescription = ({ foods, isOpen, deleteMeal }: MealDescriptionProps) => {
+export const MealDescription = ({ foods }: MealDescriptionProps) => {
 
   const UNIT: Record<MealFood['unit'], string> = {
     "GRAMS": "g",
@@ -18,10 +13,7 @@ export const MealDescription = ({ foods, isOpen, deleteMeal }: MealDescriptionPr
   }
 
   return (
-    <div
-      style={{ display: isOpen ? "flex" : "none" }}
-      className="flex flex-col gap-2 p-4 border-t-2"
-    >
+    <>
       <h3 className=" text-lg text-center font-semibold text-sky-600">Lista de alimentos</h3>
       <ul className="flex flex-col gap-2 rounded-lg list-disc pl-8 py-2" >
         {foods?.map((foodItem) => {
@@ -32,23 +24,6 @@ export const MealDescription = ({ foods, isOpen, deleteMeal }: MealDescriptionPr
           );
         })}
       </ul>
-
-      <Button
-        type="button"
-        className="w-full bg-white text-sky-700 border-2 border-sky-700"
-      >
-        Ver mais detalhes
-      </Button>
-      <div className="grid grid-cols-2 gap-2">
-        <Button
-          type="button"
-          className="w-full bg-sky-700 hover:bg-sky-500"
-          onClick={() => console.log(`/edit`)}
-        >
-          Editar refeição
-        </Button>
-        <DeleteButton onDelete={deleteMeal} />
-      </div>
-    </div>
+    </>
   );
 };
--- src/pages/Meals/MyMeals/components/meal-item.tsx
+++ src/pages/Meals/MyMeals/components/meal-item.tsx
@@ -2,43 +2,42 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useToggleState } from "@/hooks/useToggleState";
 
 import { getFoodsFromMeal } from "@/services/http/food/get-foods-from-meal";
+import { deleteMeal } from "@/services/http/meal/delete-meal";
 import { calculateTotalCalories } from "@/functions/calculate-total-calories";
 
-import { MealDescription } from "./meal-description";
 import { ChevronButton } from "./chevron-button";
+import { CaloriesBadge } from "@/components/calories-badge";
+import { MealDescription } from "./meal-description";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { DeleteButton } from "./delete-button";
 
 import { Meal } from "@/types/types";
-import { CaloriesBadge } from "@/components/calories-badge";
-import { deleteMeal } from "@/services/http/meal/delete-meal";
 
 interface MealItemProps {
   meal: Meal;
 }
 
-export const MealItem = ({ meal: { id, name } }: MealItemProps) => {
+export const MealItem = ({ meal }: MealItemProps) => {
 
   const { booleanExp, toggleBooleanExp } = useToggleState();
+  
+  const queryClient = useQueryClient()
 
   const { data: foods } = useQuery({
-    queryKey: [`foodsFromMeal-${id}`],
-    queryFn: () => getFoodsFromMeal(id),
+    queryKey: [`foodsFromMeal-${meal.id}`],
+    queryFn: () => getFoodsFromMeal(meal.id),
   })
-
-  const queryClient = useQueryClient()
 
   const { mutateAsync: deleteMealFn } = useMutation({
     mutationFn: deleteMeal,
     onSuccess() {
       queryClient.setQueryData(
         ["mealsList"],
-        (data: Meal[]) => data.filter((meal) => (meal.id !== id))
+        (data: Meal[]) => data.filter((mealItem) => (mealItem.id !== meal.id))
       )
     },
   })
-
-  const handleDeleteMeal = () => {
-    deleteMealFn(id)
-  }
 
   return (
     <li
@@ -47,11 +46,30 @@ export const MealItem = ({ meal: { id, name } }: MealItemProps) => {
       <div className=" w-full flex gap-2 items-center py-2 px-1" onClick={toggleBooleanExp}>
         <ChevronButton isOpen={booleanExp} />
         <h2 className=" w-full text-lg font-semibold text-sky-700">
-          {name}
+          {meal.name}
         </h2>
         <CaloriesBadge calories={calculateTotalCalories(foods)} />
       </div>
-      <MealDescription isOpen={booleanExp} foods={foods} deleteMeal={handleDeleteMeal} />
+
+      <div
+        style={{ display: booleanExp ? "flex" : "none" }}
+        className="flex flex-col gap-2 p-4 border-t-2 border-gray-300"
+      >
+        <MealDescription foods={foods} />
+
+        <Button className="w-full bg-white text-sky-700 border-2 border-sky-700">
+          Ver mais detalhes
+        </Button>
+
+        <div className="grid grid-cols-2 gap-2">
+          <Link to={`edit-meal/${meal.id}`}>
+            <Button className="w-full bg-sky-700 hover:bg-sky-500" >
+              Editar refeição
+            </Button>
+          </Link>
+          <DeleteButton onDelete={() => deleteMealFn(meal.id)} />
+        </div>
+      </div>
     </li>
   );
 };

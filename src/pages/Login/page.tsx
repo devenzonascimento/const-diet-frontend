649e6d1499f486de480f3d34bfdab32d8fc7baed
devenzonascimento <dev.enzody@gmail.com>
2024-06-02 15:00:34 -0300

    refactor: remove image from login page

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -6,8 +6,7 @@ export const LoginPage = () => {
   //console.log(userId, token)
 
   return (
-    <main className="h-screen w-screen flex flex-col items-center gap-8 bg-slate-200">
-      <img className="mt-20" src="/assets/logo.svg" alt="" />
+    <main className="h-screen w-screen flex flex-col justify-center items-center gap-8 bg-slate-200">
       <p className="text-3xl font-semibold text-center tracking-wider uppercase text-gray-900">Seja bem-vindo</p>
       <LoginForm />
     </main>

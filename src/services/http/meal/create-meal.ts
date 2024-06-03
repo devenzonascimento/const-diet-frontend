88eb6f985aff2e6b8e2e3ba084f1d882b28f42ab
devenzonascimento <dev.enzody@gmail.com>
2024-06-03 15:56:47 -0300

    refactor: add verifying and error handler

--- src/services/http/meal/create-meal.ts
+++ src/services/http/meal/create-meal.ts
@@ -11,6 +11,13 @@ interface CreateMeal {
 
 export const createMeal = async (mealData: CreateMeal) => {
 
+  if (mealData.name == "") {
+    throw new Error ("O nome da refeição é obrigatório!")
+  }
+  if (mealData.foods.length == 0) {
+    throw new Error ("Uma refeição precisa ter no mínimo um alimento!")
+  }
+
   const userId = localStorage.getItem("userId")
 
   const response = await api.post(`/users/${userId}/meals`, mealData)

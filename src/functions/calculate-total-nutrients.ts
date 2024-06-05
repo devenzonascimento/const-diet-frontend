80d7c6ab1fc6634c832eb983a51e078dc075c069
devenzonascimento <dev.enzody@gmail.com>
2024-06-04 21:59:40 -0300

    feat: adapted function to receive sodium and fiber properties

--- src/functions/calculate-total-nutrients.ts
+++ src/functions/calculate-total-nutrients.ts
@@ -4,7 +4,9 @@ export const calculateTotalNutrients = (foods: MealFood[] | undefined) => {
   const totalNutrients = {
     "carbohydrates": 0,
     "proteins": 0,
-    "fats": 0
+    "fats": 0,
+    "sodiums": 0,
+    "fibers": 0
   }
 
   foods?.forEach((item) => {
@@ -12,10 +14,14 @@ export const calculateTotalNutrients = (foods: MealFood[] | undefined) => {
     const carbohydrates = item.quantity * item.food?.carbohydrates / 100
     const proteins = item.quantity * item.food?.proteins / 100
     const fats = item.quantity * item.food?.fats / 100
+    const sodiums = item.quantity * item.food?.sodiums / 100
+    const fibers = item.quantity * item.food?.fibers / 100
 
     totalNutrients["carbohydrates"] = totalNutrients["carbohydrates"] + carbohydrates
     totalNutrients["proteins"] = totalNutrients["proteins"] + proteins
     totalNutrients["fats"] = totalNutrients["fats"] + fats
+    totalNutrients["sodiums"] = totalNutrients["sodiums"] + sodiums
+    totalNutrients["fibers"] = totalNutrients["fibers"] + fibers
   })
 
   return totalNutrients;

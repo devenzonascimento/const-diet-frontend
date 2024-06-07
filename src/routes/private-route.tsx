4a128934268be6d14698f01e9e3e22a906c5c427
devenzonascimento <dev.enzody@gmail.com>
2024-06-07 15:38:34 -0300

    feat: improve private route and add a method to invalidate the query when te route change

--- src/routes/private-route.tsx
+++ src/routes/private-route.tsx
@@ -1,37 +1,47 @@
-import { LoginPage } from '@/pages/Login/page';
+import { ReactNode, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
 import { checkAuthentication } from '@/services/http/check-authentication/check-authentication';
-import { useQuery } from '@tanstack/react-query';
-import { ReactNode, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-
 
 interface PrivateRouteProps {
   children: ReactNode;
 }
 
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
-
   const navigate = useNavigate();
+  const location = useLocation();
+  const queryClient = useQueryClient();
 
-  const { isSuccess, isError, isPending } = useQuery({
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ["auth"] });
+  }, [location, queryClient]);
+
+  const { isSuccess, isError, isLoading } = useQuery({
     queryKey: ["auth"],
     queryFn: checkAuthentication,
-  })
+    retry: false
+  });
 
-  if (isPending) {
+  useEffect(() => {
+    if (isError) {
+      navigate("/login");
+    }
+  }, [isError, navigate]);
+
+  if (isLoading) {
     return (
       <div className='h-screen flex justify-center items-center'>
-        <h1 className='w-60 text-center font-semibold text-2xl text-sky-950 p-2 border-4 border-sky-700 rounded-xl'>Aguarde enquanto verificamos sua autorização...</h1>
+        <h1 className='w-60 text-center font-semibold text-2xl text-sky-950 p-2 border-4 border-sky-700 rounded-xl'>
+          Aguarde enquanto verificamos sua autorização...
+        </h1>
       </div>
-    )
-  }
-
-  if (isError) {
-    return navigate("/login")
+    );
   }
 
   if (isSuccess) {
-    return children;
+    return <>{children}</>;
   }
+
+  return null;
 };

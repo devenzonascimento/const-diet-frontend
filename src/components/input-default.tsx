0b59f98bab3cdb54561802abc04cee0b7f00da69
devenzonascimento <dev.enzody@gmail.com>
2024-06-08 14:00:45 -0300

    refactor: change the component name and styles

--- src/components/input-default.tsx
+++ src/components/input-default.tsx
@@ -1,23 +1,22 @@
-type LabelProps = {
+import { InputHTMLAttributes, forwardRef } from "react";
+
+type Props = {
   label: string
   errorMessage?: string
 }
 
-type  InputDefaultProps = InputHTMLAttributes<HTMLInputElement> & LabelProps
+type  DefaultInputProps = InputHTMLAttributes<HTMLInputElement> & Props
 
-import { InputHTMLAttributes, forwardRef } from "react";
-
-export const InputDefault = forwardRef<HTMLInputElement, InputDefaultProps>(
-  ({ label = "", type = "text", name = "", errorMessage = "", ...props }, ref) => {
+export const DefaultInput = forwardRef<HTMLInputElement, DefaultInputProps>(
+  ({ label = "", type = "text", errorMessage = "", ...props }, ref) => {
     return (
-      <fieldset className="w-full flex flex-col">
-        <label className="font-semibold text-xl text-gray-800" htmlFor={props.id}>
+      <fieldset className="w-full flex flex-col gap-2">
+        <label className="font-semibold text-lg text-gray-800" htmlFor={props.id}>
           {label}
         </label>
         <input
-          className="text-lg p-1 p bg-transparent border-b border-gray-400"
+          className="w-full text-lg py-1 px-2 border rounded-md placeholder:text-gray-700"
           type={type}
-          name={name}
           ref={ref}
           {...props}
         />

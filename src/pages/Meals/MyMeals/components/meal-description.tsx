f138c749f9a43a0e41beb83def8bbccefdf9af8f
devenzonascimento <dev.enzody@gmail.com>
2024-06-08 20:18:16 -0300

    fix: adapt to new format data

--- src/pages/Meals/MyMeals/components/meal-description.tsx
+++ src/pages/Meals/MyMeals/components/meal-description.tsx
@@ -11,10 +11,10 @@ export const MealDescription = ({ foods }: MealDescriptionProps) => {
     <>
       <h3 className=" text-lg text-center font-semibold text-sky-600">Lista de alimentos</h3>
       <ul className="flex flex-col gap-2 rounded-lg list-disc pl-8 py-2" >
-        {foods?.map((foodItem) => {
+        {foods?.map(({food, quantity}) => {
           return (
-            <li key={foodItem.food.id} className="w-full p-1 text-sky-700">
-              {`${foodItem.quantity}${UNIT[foodItem.unit]} de ${foodItem.food.name}`}
+            <li key={food.id} className="w-full p-1 text-sky-700">
+              {`${quantity}${UNIT[food.unit]} de ${food.name}`}
             </li>
           );
         })}

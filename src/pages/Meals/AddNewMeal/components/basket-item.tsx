141b46fb2c2259235863250d98e22ebe59783055
devenzonascimento <dev.enzody@gmail.com>
2024-06-10 17:40:39 -0300

    refactor: adapt component to changes in meal food properties

--- src/pages/Meals/AddNewMeal/components/basket-item.tsx
+++ src/pages/Meals/AddNewMeal/components/basket-item.tsx
@@ -4,26 +4,25 @@ import { Button } from "@/components/ui/button";
 
 import { Trash2 } from "lucide-react";
 
-import { MealFood } from "@/types/types";
+import { Food, MealFood } from "@/types/types";
 
 interface FoodItemProps {
   foodItem: MealFood;
 }
 
-export const BasketItem = ({ foodItem: { quantity, unit, food } }: FoodItemProps) => {
+export const BasketItem = ({ foodItem: { quantity, food } }: FoodItemProps) => {
 
   const { removeFoodFromFoodList } = useMealContext()
 
-  const UNIT: Record<MealFood['unit'], string> = {
+  const UNIT: Record<Food['unit'], string> = {
     "GRAMS": "g",
     "MILILITERS": "ml",
-    "UNITS": "u",
   }
 
   return (
     <li className="w-full flex justify-between items-center pb-2 border-b border-gray-300">
       <p className="w-64 font-medium text-sky-700">
-        {`${quantity}${UNIT[unit]} de ${food.name}`}
+        {`${quantity}${UNIT[food.unit]} de ${food.name}`}
       </p>
       <Button
         size={"icon"}

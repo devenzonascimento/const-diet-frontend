970ad45d4d6d8992ccec845b1ab727d87b27d497
devenzonascimento <dev.enzody@gmail.com>
2024-06-27 15:25:44 -0300

    feat: add a routines list (initial)

--- src/pages/Routines/MyRoutines/page.tsx
+++ src/pages/Routines/MyRoutines/page.tsx
@@ -1,7 +1,23 @@
-import { ArrowLeft, CirclePlus } from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
+import { getRoutinesList } from "@/services/http/routine/get-routine-list"
+
+import { ArrowLeft, CirclePlus } from "lucide-react"
+import { List } from "@/components/list"
+
 export const MyRoutinesPage = () => {
+
+  const { data: routinesList, isError } = useQuery({
+    queryKey: ["routinesList"],
+    queryFn: getRoutinesList,
+    refetchOnMount: false,
+  })
+
+  if (isError || !routinesList) {
+    return <div>Erro ao carregar rotinas</div>
+  }
+
   return (
     <div className="h-screen bg-slate-100 px-4">
       <header className="relative flex justify-center items-center py-4 text-sky-950">
@@ -9,12 +25,15 @@ export const MyRoutinesPage = () => {
           <ArrowLeft size={32} className="absolute top-4 left-0" />
         </Link>
         <h1 className="text-xl font-semibold">Minhas rotinas</h1>
-        <Link to="/">
+        <Link to="/add-new-routine">
           <CirclePlus size={32} className="absolute top-4 right-0" />
         </Link>
       </header>
       <main className="flex flex-col justify-between items-center gap-4 pb-6">
-        
+        <List
+          data={routinesList}
+          renderItem={({ item }) => <div key={item.id}>{item.name}</div>}
+        />
       </main>
     </div>
   )

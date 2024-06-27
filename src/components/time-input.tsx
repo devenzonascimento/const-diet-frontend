0d42dfbef90b220ffe5ff637574c15bee3ef18cb
devenzonascimento <dev.enzody@gmail.com>
2024-06-27 13:58:50 -0300

    feat: transform TimeInput in a controlled component

--- src/components/time-input.tsx
+++ src/components/time-input.tsx
@@ -2,13 +2,13 @@ import { ChevronDown, ChevronUp } from "lucide-react";
 import { useEffect, useReducer } from "react";
 
 interface TimeState {
-  hours: number | string;
-  minutes: number | string;
+  hours: number;
+  minutes: number;
 }
 
 const initialState: TimeState = {
-  hours: "00",
-  minutes: "00"
+  hours: new Date().getHours(),
+  minutes: new Date().getMinutes()
 };
 
 type Action =
@@ -26,32 +26,26 @@ const timeReducer = (state: TimeState, action: Action): TimeState => {
     case 'SET_MINUTES':
       return { ...state, minutes: action.payload };
     case 'INCREMENT_HOURS':
-      return { ...state, hours: (Number(state.hours) + 1) % 24 };
+      return { ...state, hours: (state.hours + 1) % 24 };
     case 'DECREMENT_HOURS':
-      return { ...state, hours: (Number(state.hours) - 1 + 24) % 24 };
+      return { ...state, hours: (state.hours - 1 + 24) % 24 };
     case 'INCREMENT_MINUTES': {
-      const newMinutes = Number(state.minutes) + 1;
+      const newMinutes = state.minutes + 1;
       const incrementHours = newMinutes >= 60 ? 1 : 0;
 
-      const hours = (Number(state.hours) + incrementHours) % 24
+      const hours = (state.hours + incrementHours) % 24
       const minutes = newMinutes % 60
 
-      return { 
-        hours: hours.toString().padStart(2, '0'),
-        minutes: minutes.toString().padStart(2, '0') 
-      };
+      return { hours, minutes };
     }
     case 'DECREMENT_MINUTES': {
-      const newMinutes = Number(state.minutes) - 1;
+      const newMinutes = state.minutes - 1;
       const decrementHours = newMinutes < 0 ? -1 : 0;
 
-      const hours = (Number(state.hours) + decrementHours + 24) % 24
+      const hours = (state.hours + decrementHours + 24) % 24
       const minutes = (newMinutes + 60) % 60
-
-      return { 
-        hours: hours.toString().padStart(2, '0'),
-        minutes: minutes.toString().padStart(2, '0') 
-      };
+      
+      return { hours, minutes };
     }
     default:
       return state;
@@ -66,55 +60,13 @@ export const TimeInput = ({ setTime }: TimeInputProps) => {
   const [state, dispatch] = useReducer(timeReducer, initialState);
 
   useEffect(() => {
-    const hours = state.hours
-    const minutes = state.minutes
+    const hours = state.hours.toString().padStart(2, '0')
+    const minutes = state.minutes.toString().padStart(2, '0')
 
     setTime(`${hours}:${minutes}`)
   }, [state, setTime])
 
   return (
-    <div className="flex items-center gap-1">
-      <fieldset className="w-14 flex flex-col items-center border-2 rounded-md">
-        <button type="button" onClick={() => dispatch({ type: 'INCREMENT_HOURS' })}>
-          <ChevronUp size={32} />
-        </button>
-        <input
-          value={state.hours}
-          maxLength={2}
-          className="w-full border-y-2 px-2 py-1 text-2xl text-center font-semibold"
-          onChange={({ target }) => dispatch({ 
-            type: "SET_HOURS", 
-            payload: Number(target.value)
-          })}
-        />
-        <button type="button" onClick={() => dispatch({ type: 'DECREMENT_HOURS' })}>
-          <ChevronDown size={32} />
-        </button>
-      </fieldset>
-      <span className="text-xl font-bold">:</span>
-      <fieldset className="w-14 flex flex-col items-center border-2 rounded-md">
-        <button type="button" onClick={() => dispatch({ type: 'INCREMENT_MINUTES' })}>
-          <ChevronUp size={32} />
-        </button>
-        <input
-          value={state.minutes}
-          maxLength={2}
-          className="w-full border-y-2 px-2 py-1 text-2xl text-center font-semibold"
-          onChange={({ target }) => dispatch({ 
-            type: "SET_MINUTES", 
-            payload: Number(target.value)
-          })}
-        />
-        <button type="button" onClick={() => dispatch({ type: 'DECREMENT_MINUTES' })}>
-          <ChevronDown size={32} />
-        </button>
-      </fieldset>
-    </div>
-  )
-}
-
-/*
-return (
     <div className="flex items-center gap-1">
       <fieldset className="w-14 flex flex-col items-center border-2 rounded-md">
         <button type="button" onClick={() => dispatch({ type: 'INCREMENT_HOURS' })}>
@@ -141,4 +93,4 @@ return (
       </fieldset>
     </div>
   )
-*/
+}

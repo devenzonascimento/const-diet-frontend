98a50177671f774a43e96641823e0c56044a45bb
devenzonascimento <dev.enzody@gmail.com>
2024-06-28 18:37:28 -0300

    feat: add a search input in routines list

--- src/pages/Routines/MyRoutines/page.tsx
+++ src/pages/Routines/MyRoutines/page.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
@@ -6,8 +7,11 @@ import { getRoutinesList } from "@/services/http/routine/get-routine-list"
 import { ArrowLeft, CirclePlus } from "lucide-react"
 import { List } from "@/components/list"
 import { RoutineItem } from "./components/routine-item"
+import { SearchInput } from "@/components/search-input"
 
 export const MyRoutinesPage = () => {
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   const { data: routinesList, isError } = useQuery({
     queryKey: ["routinesList"],
@@ -18,6 +22,10 @@ export const MyRoutinesPage = () => {
   if (isError || !routinesList) {
     return <div>Erro ao carregar rotinas</div>
   }
+
+  const filteredRoutine = routinesList?.filter(routine =>
+    routine.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="h-screen bg-slate-100 px-4">
@@ -31,8 +39,13 @@ export const MyRoutinesPage = () => {
         </Link>
       </header>
       <main className="flex flex-col justify-between items-center gap-4 pb-6">
+        <SearchInput 
+          placeholder="Buscar rotina"
+          value={searchTerm}
+          onChange={({ target }) => setSearchTerm(target.value)}
+        />
         <List
-          data={routinesList}
+          data={filteredRoutine}
           renderItem={({ item }) => <RoutineItem key={item.id} routine={item} />}
         />
       </main>

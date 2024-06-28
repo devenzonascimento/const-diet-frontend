caedf3f107f6b6ef8cebfb646da51d5a53f3571f
devenzonascimento <dev.enzody@gmail.com>
2024-06-27 23:34:46 -0300

    refactor: simplify code

--- src/functions/calculate-total-nutrients.ts
+++ src/functions/calculate-total-nutrients.ts
@@ -17,11 +17,11 @@ export const calculateTotalNutrients = (foods: MealFood[] | undefined) => {
     const sodiums = item.quantity * item.food?.sodiums / 100
     const fibers = item.quantity * item.food?.fibers / 100
 
-    totalNutrients["carbohydrates"] = totalNutrients["carbohydrates"] + carbohydrates
-    totalNutrients["proteins"] = totalNutrients["proteins"] + proteins
-    totalNutrients["fats"] = totalNutrients["fats"] + fats
-    totalNutrients["sodiums"] = totalNutrients["sodiums"] + sodiums
-    totalNutrients["fibers"] = totalNutrients["fibers"] + fibers
+    totalNutrients.carbohydrates = totalNutrients.carbohydrates + carbohydrates
+    totalNutrients.proteins = totalNutrients.proteins + proteins
+    totalNutrients.fats = totalNutrients.fats + fats
+    totalNutrients.sodiums = totalNutrients.sodiums + sodiums
+    totalNutrients.fibers = totalNutrients.fibers + fibers
   })
 
   return totalNutrients;

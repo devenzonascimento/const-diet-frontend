6093e5b09d6f70ad8a1efe2c9ad94547b781a680
devenzonascimento <dev.enzody@gmail.com>
2024-06-29 14:40:26 -0300

    feat: add mealBasketItem component (initial)

---
+++ src/pages/Routines/_components/meal-basket-item.tsx
@@ -0,0 +1,26 @@
+import { useRoutineContext } from "@/context/routine-context"
+import { DailyMeal } from "@/types/types"
+import { Trash2Icon } from "lucide-react"
+
+interface MealBasketItemProps {
+  dailyMeal: DailyMeal
+}
+
+export const MealBasketItem = ({ dailyMeal }: MealBasketItemProps) => {
+
+  const { removeMeal } = useRoutineContext()
+
+  return (
+    <li className="h-16 flex items-center justify-between gap-1.5 bg-white rounded-sm overflow-hidden shadow-lg">
+      <span className=" h-full flex items-center px-1.5 bg-sky-700 text-white font-semibold">
+        {dailyMeal.time}
+      </span>
+      <span className="w-full text-sky-800 font-semibold break-words">
+        {dailyMeal.meal.name}
+      </span>
+      <button onClick={() => removeMeal(dailyMeal)} className="h-full px-2 bg-red-600 ">
+        <Trash2Icon className="text-white" />
+      </button>
+    </li>
+  )
+}

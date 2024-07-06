eb6bd69622f6a10546be7deed23bc1cdfa24b9c0
devenzonascimento <dev.enzody@gmail.com>
2024-07-06 16:18:02 -0300

    feat: add index property in list component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -2,15 +2,15 @@ import { cn } from "@/lib/utils";
 
 interface ListProps<T> {
   data: T[];
-  renderItem: ({ item }: { item: T }) => React.ReactNode;
+  renderItem: ({ item, index }: { item: T, index: number }) => React.ReactNode;
   className?: string;
 }
 
 export const List = <T,>({ data, renderItem, className }: ListProps<T>) => {
   return (
     <ul className={cn("w-full flex flex-col gap-6 overflow-auto", className)}>
-      {data.map((item) => (
-        renderItem({ item })
+      {data.map((item, index) => (
+        renderItem({ item, index })
       ))}
     </ul>
   );

3dc0fbddb2f83e0e7121aff17c7826e82bc9d297
devenzonascimento <dev.enzody@gmail.com>
2024-07-05 21:09:49 -0300

    feat: add DateInput component (initial)

---
+++ src/components/date-input.tsx
@@ -0,0 +1,49 @@
+import { format } from "date-fns"
+
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+import { Calendar as CalendarIcon } from "lucide-react"
+
+interface DateInputProps {
+  label: string;
+  date: Date | undefined
+  setDate: (newDate: Date) => void
+}
+
+export function DateInput({ label, date, setDate }: DateInputProps) {
+  return (
+    <fieldset className="w-full flex flex-col gap-2">
+      <p className="font-semibold text-lg text-sky-950">
+        {label}
+      </p>
+      <Popover>
+        <PopoverTrigger asChild>
+          <div
+            id="date"
+            className="w-full flex items-center text-lg py-1 px-2 border border-sky-900 rounded-md text-left font-normal bg-white"
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ?
+              format(date, "dd/LL/y")
+              :
+              <span>Pick a date</span>
+            }
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+          />
+        </PopoverContent>
+      </Popover>
+    </fieldset>
+  )
+}

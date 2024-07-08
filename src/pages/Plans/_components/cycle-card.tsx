536b4fb922ad09edd19aa4bf7cc42782f3e4679d
devenzonascimento <dev.enzody@gmail.com>
2024-07-08 18:09:42 -0300

    feat: add a option to insert more slots when isCustom is active, and add date indicator

--- src/pages/Plans/_components/cycle-card.tsx
+++ src/pages/Plans/_components/cycle-card.tsx
@@ -5,6 +5,11 @@ import { useEffect } from "react";
 import { ModalBackdrop } from "@/components/modal-backdrop";
 import { List } from "@/components/list";
 import { RoutineSlot } from "./routine-slot";
+import { addDays, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+import { Button } from "@/components/ui/button";
+
 
 interface CycleCardProps {
   title: string;
@@ -18,12 +23,14 @@ export const CycleCard = ({
   title,
   description,
   numberOfSlots = 1,
+  isCustom,
   onClose
 }: CycleCardProps) => {
 
-  const { routinesCycle, setRoutinesCycle } = usePlanContext()
+  const { routinesCycle, setRoutinesCycle, startDateValue } = usePlanContext()
 
   useEffect(() => {
+    /*
     setRoutinesCycle([
       {
         id: "123c",
@@ -38,7 +45,8 @@ export const CycleCard = ({
         meals: []
       }
     ])
-    //setRoutinesCycle(Array.from({ length: numberOfSlots }))
+    */
+    setRoutinesCycle(Array.from({ length: numberOfSlots }))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -46,16 +54,32 @@ export const CycleCard = ({
     <>
       <ModalBackdrop onClose={onClose} >
         <div
-          className="h-[70%] w-[90%] flex flex-col gap-2 items-center p-2 bg-white border-4 border-sky-400 rounded-xl"
+          className="h-[80%] w-[90%] flex flex-col gap-2 items-center p-2 bg-white border-4 border-sky-400 rounded-xl"
         >
           <h1 className="font-semibold text-lg text-sky-950">{title}</h1>
           <p className="text-sm text-slate-500">{description}</p>
           <List
             data={routinesCycle}
-            renderItem={({ item, index }) =>
-              <RoutineSlot key={index} routine={item} slot={index} />
-            }
+            renderItem={({ item, index }) => (
+              <div>
+                <p className="pl-1 font-semibold text-sky-950">
+                  {index + 1}º dia - {format(addDays(startDateValue, index), "E, dd 'de' LLLL 'de' y", { locale: ptBR })}
+                </p>
+                <RoutineSlot key={index} routine={item} slot={index} />
+              </div>
+            )}
           />
+          {isCustom &&
+            <Button
+              type="submit"
+              onClick={() => {
+                setRoutinesCycle([...routinesCycle, undefined])
+              }}
+              className="w-full flex gap-2 bg-sky-700 hover:bg-sky-500"
+            >
+              Adicionar novo slot
+            </Button>
+          }
         </div>
       </ModalBackdrop>
     </>

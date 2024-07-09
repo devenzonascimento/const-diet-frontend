3ec35da1432868f7d48214c451b30ac576d38422
devenzonascimento <dev.enzody@gmail.com>
2024-07-09 16:20:16 -0300

    feat: add a http function to create a plan

---
+++ src/services/http/plan/create-plan.ts
@@ -0,0 +1,24 @@
+import { api } from "@/services/api";
+
+interface PlanCreate {
+  name: string;
+  goal: string;
+  startDate: Date;
+  endDate: Date;
+  routines: {
+    routineId: string;
+    dates: Date[];
+  }[];
+}
+
+export const createPlan = async (planData: PlanCreate) => {
+  try {
+    const userId = localStorage.getItem("userId");
+
+    const { data } = await api.post(`/users/${userId}/plans`, planData);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};

73f5b05925bea0aeb3e8ca288e53d17c2d9c3b3f
devenzonascimento <dev.enzody@gmail.com>
2024-07-11 16:07:06 -0300

    feat: implements RoutineItem component in RoutineSlot

--- src/pages/Plans/_components/routine-slot.tsx
+++ src/pages/Plans/_components/routine-slot.tsx
@@ -1,7 +1,8 @@
 import { usePlanContext } from "@/context/plan-context";
 import { useModalState } from "@/hooks/use-modal-state";
 
-import { Trash2Icon, PlusCircleIcon, FlameIcon, DropletIcon } from "lucide-react";
+import { RoutineItem } from "./routine-item";
+import { PlusCircleIcon } from "lucide-react";
 import { When } from "@/components/when";
 import { SelectRoutinesCard } from "./select-routines-card";
 
@@ -26,66 +27,7 @@ export const RoutineSlot = ({ routine, slot }: RoutineSlotProps) => {
     <>
       {routine ?
         (
-          <li className="min-h-40 w-full flex border-4 border-sky-800 rounded-xl shadow-xl">
-            <div className="w-full flex-1 flex flex-col gap-2 p-2">
-              <header className="flex items-center gap-1">
-                <h2 className="text-lg font-semibold text-sky-950 truncate">
-                  {routine.name}
-                </h2>
-                <button
-                  onClick={() => removeRoutine(slot)}
-                  className="ml-auto bg-red-500 p-1 rounded-lg"
-                >
-                  <Trash2Icon className="text-white" />
-                </button>
-              </header>
-              <main className="flex flex-col gap-4">
-                <section className="flex justify-between gap-4">
-                  <div
-                    className="flex-1 flex flex-col items-center justify-cente overflow-auto"
-                  >
-                    <div className="w-full flex items-center justify-center gap-1 p-1 border-2 border-sky-800 rounded-t-xl">
-                      <span className=" text-sky-950 font-semibold">Calorias</span>
-                      <FlameIcon size={18} className="text-sky-800" />
-                    </div>
-                    <p className="w-full flex items-center justify-center font-semibold p-1 bg-sky-800 text-white rounded-b-xl">
-                      {routine.totalCalories.toFixed()}
-                      <span className="text-xs">kcal</span>
-                    </p>
-                  </div>
-                  <div
-                    className="flex-1 flex flex-col items-center justify-cente overflow-auto"
-                  >
-                    <div className="w-full flex items-center justify-center gap-1 p-1 border-2 border-sky-800 rounded-t-xl">
-                      <span className=" text-sky-950 font-semibold">Agua</span>
-                      <DropletIcon size={18} className="text-sky-800" />
-                    </div>
-                    <p className="w-full flex items-center justify-center font-semibold p-1 bg-sky-800 text-white rounded-b-xl">
-                      {routine.water}
-                      <span className="text-xs">ml</span>
-                    </p>
-                  </div>
-                </section>
-                <ul className="flex items-center justify-around gap-1 ">
-                  <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
-                    C {routine.totalCarbohydrates.toFixed()}g
-                  </li>
-                  <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
-                    P {routine.totalProteins.toFixed()}g
-                  </li>
-                  <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
-                    G {routine.totalFats.toFixed()}g
-                  </li>
-                  <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
-                    S {routine.totalSodiums.toFixed(3)}g
-                  </li>
-                  <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
-                    F {routine.totalFibers.toFixed()}g
-                  </li>
-                </ul>
-              </main>
-            </div>
-          </li>
+          <RoutineItem routine={routine} onDelete={() => removeRoutine(slot)}/>
         )
         :
         (

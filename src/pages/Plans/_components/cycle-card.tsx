0bae45fc5e4362cce626903afcc865d35c65dece
devenzonascimento <dev.enzody@gmail.com>
2024-07-11 15:03:41 -0300

    feat: disable button while all the slots are not filled

--- src/pages/Plans/_components/cycle-card.tsx
+++ src/pages/Plans/_components/cycle-card.tsx
@@ -1,5 +1,5 @@
 import { usePlanContext } from "@/context/plan-context";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 import { ModalBackdrop } from "@/components/modal-backdrop";
@@ -30,25 +30,19 @@ export const CycleCard = ({
   const { routinesCycle, setRoutinesCycle, startDateValue } = usePlanContext()
 
   useEffect(() => {
-    /*
-    setRoutinesCycle([
-      {
-        id: "123c",
-        name: "Rotina para testes",
-        water: 3750,
-        totalCalories: 3250,
-        totalCarbohydrates: 225,
-        totalProteins: 180,
-        totalFats: 80,
-        totalSodiums: 1,
-        totalFibers: 10,
-        meals: []
-      }
-    ])
-    */
     setRoutinesCycle(Array.from({ length: numberOfSlots }))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const [existEmptySlot, setExistEmptySlot] = useState<boolean>(true)
+
+  useEffect(() => {    
+    setExistEmptySlot(routinesCycle.some(routineCycle => routineCycle === undefined))
+  }, [routinesCycle, setRoutinesCycle])
+
+  const handleSave = () => {
+    onClose()
+  }
 
   return (
     <>
@@ -56,30 +50,42 @@ export const CycleCard = ({
         <div
           className="h-[80%] w-[90%] flex flex-col gap-2 items-center p-2 bg-white border-4 border-sky-400 rounded-xl"
         >
-          <h1 className="font-semibold text-lg text-sky-950">{title}</h1>
-          <p className="text-sm text-slate-500">{description}</p>
-          <List
-            data={routinesCycle}
-            renderItem={({ item, index }) => (
-              <div>
-                <p className="pl-1 font-semibold text-sky-950">
-                  {index + 1}º dia - {format(addDays(startDateValue, index), "E, dd 'de' LLLL 'de' y", { locale: ptBR })}
-                </p>
-                <RoutineSlot key={index} routine={item} slot={index} />
-              </div>
-            )}
-          />
-          {isCustom &&
-            <Button
-              type="submit"
-              onClick={() => {
-                setRoutinesCycle([...routinesCycle, undefined])
-              }}
-              className="w-full flex gap-2 bg-sky-700 hover:bg-sky-500"
-            >
-              Adicionar novo slot
-            </Button>
-          }
+          <header className="flex flex-col gap-2 items-center">
+            <h1 className="font-semibold text-lg text-sky-950">{title}</h1>
+            <p className="text-sm text-slate-500">{description}</p>
+          </header>
+          <main className="w-full flex-1 overflow-auto">
+            <List
+              data={routinesCycle}
+              renderItem={({ item, index }) => (
+                <div key={index}>
+                  <p className="pl-1 font-semibold text-sky-950">
+                    {index + 1}º dia - {format(addDays(startDateValue, index), "E, dd 'de' LLLL 'de' y", { locale: ptBR })}
+                  </p>
+                  <RoutineSlot routine={item} slot={index} />
+                </div>
+              )}
+            />
+            {isCustom &&
+              <Button
+                type="submit"
+                onClick={() => {
+                  setRoutinesCycle([...routinesCycle, undefined])
+                }}
+                className="w-full flex gap-2 bg-sky-700 hover:bg-sky-500"
+              >
+                Adicionar novo slot
+              </Button>
+            }
+          </main>
+          <Button
+            disabled={existEmptySlot}
+            type="submit"
+            onClick={handleSave}
+            className="w-full flex gap-2 bg-sky-700 hover:bg-sky-500"
+          >
+            Salvar ciclo
+          </Button>
         </div>
       </ModalBackdrop>
     </>

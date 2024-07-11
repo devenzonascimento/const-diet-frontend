71278857004aae9d26c386dc243928364bff2c59
devenzonascimento <dev.enzody@gmail.com>
2024-07-11 16:06:36 -0300

    feat: improve RoutineItem styles

--- src/pages/Plans/_components/routine-item.tsx
+++ src/pages/Plans/_components/routine-item.tsx
@@ -1,26 +1,79 @@
+import { DropletIcon, FlameIcon, Trash2Icon } from "lucide-react";
+
 import { Routine } from "@/types/types";
 
 interface RoutineItemProps {
   routine: Routine;
-  onClick: () => void;
+  onClick?: () => void;
+  onDelete?: () => void;
 }
 
-export const RoutineItem = ({ routine, onClick }: RoutineItemProps) => {
+export const RoutineItem = ({ routine, onClick, onDelete }: RoutineItemProps) => {
   return (
     <li
       onClick={onClick}
-      className="flex flex-col gap-4 p-2 bg-sky-400 rounded-lg"
+      className="min-h-42 w-full flex border-4 border-sky-800 rounded-xl shadow-xl"
     >
-      <header className="flex items-start justify-between gap-4">
-        <h3 className="text-sm font-semibold text-white break-words">
-          {routine.name}
-        </h3>
-      </header>
-      <div className="max-w-full flex flex-wrap gap-2">
-        <span>{`Calories ${routine.totalCalories.toFixed()}kcal`}</span>
-        <span>{`C ${routine.totalCarbohydrates.toFixed()}g`}</span>
-        <span>{`P ${routine.totalProteins.toFixed()}g`}</span>
-        <span>{`G ${routine.totalFats.toFixed()}g`}</span>
+      <div className="w-full flex-1 flex flex-col gap-2 p-2">
+        <header className="flex items-center gap-1">
+          <h2 className="text-lg font-semibold text-sky-950 truncate">
+            {routine.name}
+          </h2>
+          {onDelete && (
+
+            <button
+              onClick={onDelete}
+              className="ml-auto bg-red-500 p-1 rounded-lg"
+            >
+              <Trash2Icon className="text-white" />
+            </button>
+          )}
+        </header>
+        <main className="flex flex-col gap-4">
+          <section className="flex justify-between gap-4">
+            <div
+              className="flex-1 flex flex-col items-center justify-cente overflow-auto"
+            >
+              <div className="w-full flex items-center justify-center gap-1 p-1 border-2 border-sky-800 rounded-t-xl">
+                <span className=" text-sky-950 font-semibold">Calorias</span>
+                <FlameIcon size={18} className="text-sky-800" />
+              </div>
+              <p className="w-full flex items-center justify-center font-semibold p-1 bg-sky-800 text-white rounded-b-xl">
+                {routine.totalCalories.toFixed()}
+                <span className="text-xs">kcal</span>
+              </p>
+            </div>
+            <div
+              className="flex-1 flex flex-col items-center justify-cente overflow-auto"
+            >
+              <div className="w-full flex items-center justify-center gap-1 p-1 border-2 border-sky-800 rounded-t-xl">
+                <span className=" text-sky-950 font-semibold">Agua</span>
+                <DropletIcon size={18} className="text-sky-800" />
+              </div>
+              <p className="w-full flex items-center justify-center font-semibold p-1 bg-sky-800 text-white rounded-b-xl">
+                {routine.water}
+                <span className="text-xs">ml</span>
+              </p>
+            </div>
+          </section>
+          <ul className="flex items-center justify-around gap-1 ">
+            <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
+              C {routine.totalCarbohydrates.toFixed()}g
+            </li>
+            <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
+              P {routine.totalProteins.toFixed()}g
+            </li>
+            <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
+              G {routine.totalFats.toFixed()}g
+            </li>
+            <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
+              S {routine.totalSodiums.toFixed(3)}g
+            </li>
+            <li className="min-w-12 flex justify-center text-xs font-medium text-white bg-sky-800 p-1 rounded-lg">
+              F {routine.totalFibers.toFixed()}g
+            </li>
+          </ul>
+        </main>
       </div>
     </li>
   )

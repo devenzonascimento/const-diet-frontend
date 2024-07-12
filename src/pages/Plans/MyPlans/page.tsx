1f6cae2665393c011e1e01b7402bffd523974cc2
devenzonascimento <dev.enzody@gmail.com>
2024-07-12 16:12:23 -0300

    feat: add a list of plans in this page

--- src/pages/Plans/MyPlans/page.tsx
+++ src/pages/Plans/MyPlans/page.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom"
 
+import { getPlansList } from "@/services/http/plan/get-plans-list";
+
 import { ArrowLeft, CirclePlus } from "lucide-react"
+import { SearchInput } from "@/components/search-input";
+import { List } from "@/components/list";
+import { PlanItem } from "./components/plan-item";
 
 export const MyPlansPage = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const { data: routinesList, isError } = useQuery({
+    queryKey: ["plansList"],
+    queryFn: getPlansList,
+    refetchOnMount: false,
+  })
+
+  if (isError || !routinesList) {
+    return <div>Erro ao carregar rotinas</div>
+  }
+
+  const filteredRoutine = routinesList?.filter(routine =>
+    routine.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="h-screen bg-slate-100 px-4">
       <header className="relative flex justify-center items-center py-4 text-sky-950">
@@ -15,6 +38,15 @@ export const MyPlansPage = () => {
         </Link>
       </header>
       <main className="flex flex-col justify-between items-center gap-4 pb-6">
+        <SearchInput
+          placeholder="Buscar rotina"
+          value={searchTerm}
+          onChange={({ target }) => setSearchTerm(target.value)}
+        />
+        <List
+          data={filteredRoutine}
+          renderItem={({ item }) => <PlanItem key={item.id} plan={item} />}
+        />
       </main>
     </div>
   )

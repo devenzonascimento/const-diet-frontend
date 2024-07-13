f4f5722581ed8212f3ada86b5690ceb131d32652
devenzonascimento <dev.enzody@gmail.com>
2024-07-12 21:24:31 -0300

    feat: add try catch statement

--- src/services/http/plan/get-plan.ts
+++ src/services/http/plan/get-plan.ts
@@ -3,10 +3,15 @@ import { api } from "@/services/api";
 import { Plan } from "@/types/types";
 
 export const getPlan = async (planId: string): Promise<Plan> => {
+  try {
+    const userId = localStorage.getItem("userId");
+  
+    const { data } = await api.get(`/users/${userId}/plans/${planId}`);
+  
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {} as Plan
+  }
 
-  const userId = localStorage.getItem("userId");
-
-  const { data } = await api.get(`/users/${userId}/plans/${planId}`);
-
-  return data;
 }

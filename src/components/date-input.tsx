a336a776548956b063d1bcac2ae73d1ee58559ff
devenzonascimento <dev.enzody@gmail.com>
2024-07-15 20:13:21 -0300

    feat: add new options in props to customize DateInput component

--- src/components/date-input.tsx
+++ src/components/date-input.tsx
@@ -9,40 +9,63 @@ import {
 
 import { Calendar as CalendarIcon } from "lucide-react"
 import { SelectSingleEventHandler } from "react-day-picker";
+import { ptBR } from "date-fns/locale";
 
 interface DateInputProps {
-  label: string;
+  label?: string;
   date: Date | undefined
   setDate: (newDate: Date) => void
   popupAlign?: "start" | "center" | "end"
+  dateFormat?: "short" | "long" | "completely"
+  setRange?: { fromDate: Date, toDate: Date }
 }
 
-export function DateInput({ label, date, setDate, popupAlign="center" }: DateInputProps) {
+export function DateInput({
+  label,
+  date,
+  setDate,
+  popupAlign = "center",
+  dateFormat = "short",
+  setRange
+}: DateInputProps) {
+
+  const dateFormatMap = {
+    short: "dd/LL/y",
+    long: "dd 'de' LLLL 'de' y",
+    completely: "EEE, dd 'de' LLL 'de' y"
+  }
+
   return (
     <fieldset className="w-full flex flex-col gap-2">
-      <p className="font-semibold text-lg text-sky-950">
-        {label}
-      </p>
+      {label && (
+        <p className="font-semibold text-lg text-sky-950">
+          {label}
+        </p>
+      )}
       <Popover>
         <PopoverTrigger asChild>
           <button
             id="date"
             className="w-full flex items-center text-lg py-1 px-2 border border-sky-900 rounded-md text-left font-normal bg-white"
           >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ?
-              format(date, "dd/LL/y")
-              :
-              <span>Pick a date</span>
-            }
+            <CalendarIcon className="mr-2 h-4 w-4 text-sky-950" />
+            <span className="text-sky-950">
+              {date ?
+                format(date, dateFormatMap[dateFormat], { locale: ptBR })
+                :
+                "Pick a date"
+              }
+            </span>
           </button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align={popupAlign}>
           <Calendar
-            defaultMonth={date}          
+            weekStartsOn={1}
+            defaultMonth={date}
             initialFocus
             mode="single"
             selected={date}
+            {...setRange}
             onSelect={setDate as SelectSingleEventHandler}
           />
         </PopoverContent>

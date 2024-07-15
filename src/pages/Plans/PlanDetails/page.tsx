2646793708c0c9f0afb7631348f25241e9a4e812
devenzonascimento <dev.enzody@gmail.com>
2024-07-15 20:20:07 -0300

    feat: add fields to show plan infos, and a section to manipulate a daily routine

--- src/pages/Plans/PlanDetails/page.tsx
+++ src/pages/Plans/PlanDetails/page.tsx
@@ -3,7 +3,8 @@ import { Link, useNavigate, useParams } from "react-router-dom"
 
 import { getPlan } from "@/services/http/plan/get-plan";
 
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, CalendarFoldIcon, GoalIcon } from "lucide-react"
+import { DayViewer } from "./components/day-viewer";
 
 interface RouteParams {
   planId: string;
@@ -11,7 +12,6 @@ interface RouteParams {
 
 export const PlanDetailsPage = () => {
   const { planId } = useParams<keyof RouteParams>() as RouteParams;
-
   const navigate = useNavigate()
 
   const { data: plan, isError } = useQuery({
@@ -36,7 +36,25 @@ export const PlanDetailsPage = () => {
         </Link>
         <h1 className="text-xl font-semibold">Detalhes do seu plano</h1>
       </header>
-      <main className="flex flex-col justify-between items-center gap-4 pb-6">
+      <main className="flex flex-col justify-between items-center gap-4 pb-6">        
+        <div className="w-full flex items-center bg-sky-800 border border-sky-800 rounded-md overflow-hidden">
+          <span className="flex items-center justify-center px-2 rounded-l-sm">      
+            <CalendarFoldIcon size={24}  className=" text-white"/>
+          </span>
+          <h2 className="w-full p-2 text-start text-lg font-medium text-sky-950 bg-white">
+            {plan?.name}
+          </h2>
+        </div>
+        <div className="w-full flex items-center bg-sky-800 border border-sky-800 rounded-md overflow-hidden">
+          <span className="flex items-center justify-center px-2 rounded-l-sm">      
+            <GoalIcon size={24}  className=" text-white"/>
+          </span>
+          <p className="w-full p-2 text-start font-medium text-sky-950 bg-white">
+            Objetivo: {plan?.goal}
+          </p>
+        </div>
+
+        <DayViewer plan={plan} />
       </main>
     </div>
   )

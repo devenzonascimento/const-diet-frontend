abdd77f76282ba558622eb16707bbe85d5ada29a
devenzonascimento <dev.enzody@gmail.com>
2024-07-16 17:24:10 -0300

    feat: select a first day automatically when the component is mounted, and implements SelectDayViewer component

--- src/pages/Plans/PlanDetails/components/day-viewer.tsx
+++ src/pages/Plans/PlanDetails/components/day-viewer.tsx
@@ -2,37 +2,41 @@ import { useState } from "react";
 
 import { isEqual } from "date-fns";
 
-import { DateInput } from "@/components/date-input";
 import { DailyRoutineItem } from "./daily-routine-item";
 
 import { Plan } from "@/types/types";
+import { SelectDayViewer } from "./select-day-viewer";
 
 interface DayViewerProps {
   plan: Plan;
 }
 
 export const DayViewer = ({ plan }: DayViewerProps) => {
-  const [selectedDay, setSelectedDay] = useState<Date>(plan.startDate);
 
+  const startDate = new Date(plan.startDate)
+
+  startDate.setHours(0, 0, 0, 0)
+
+  const [selectedDay, setSelectedDay] = useState<Date>(startDate);
+  
   return (
-    <section className="w-full p-2 flex flex-col items-center gap-2 bg-white border border-sky-800 rounded-xl">
-      <DateInput
+    <>
+      <SelectDayViewer
         date={selectedDay}
         setDate={setSelectedDay}
-        dateFormat="completely"
         setRange={{
           fromDate: plan?.startDate,
           toDate: plan?.endDate
         }}
       />
-      {plan.routines.map(routine => {
-        return isEqual(routine.date, selectedDay) ?
-          <DailyRoutineItem key={routine.id} routine={routine} />
-          :
-          null
-      })}
-    </section>
+      <section className="w-full flex flex-col items-center gap-2 bg-white border border-sky-800 rounded-xl">
+        {plan.routines.map(routine => {
+          return isEqual(routine.date, selectedDay) ?
+            <DailyRoutineItem key={routine.id} routine={routine} />
+            :
+            null
+        })}
+      </section>
+    </>
   );
 }
-
-

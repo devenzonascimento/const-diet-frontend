651223e448bf253859ae84ebdeb89136cf1eee42
devenzonascimento <dev.enzody@gmail.com>
2024-07-18 00:58:03 -0300

    style: change size and colors

--- src/components/calories-badge.tsx
+++ src/components/calories-badge.tsx
@@ -9,7 +9,7 @@ interface CaloriesBadgeProps {
 export const CaloriesBadge = ({ className, calories }: CaloriesBadgeProps) => {
   return (
     <div className={cn("flex items-center gap-0.5", className)}>
-      <Flame className="text-red-600 fill-yellow-500" size={20} />
+      <Flame className="text-sky-700 fill-sky-200" size={18} />
       <p className="pt-1 text-gray-800 font-semibold">
         {calories.toFixed()}
         <span className="text-xs">kcal</span>

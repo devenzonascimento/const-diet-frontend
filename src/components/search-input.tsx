1f7257998efe648491023dcca27133b1885bbf3c
devenzonascimento <dev.enzody@gmail.com>
2024-07-17 22:03:59 -0300

    style: change border width

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -7,7 +7,7 @@ type SearchInputProps = InputHTMLAttributes<HTMLInputElement>
 export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
   ({ ...props }, ref) => {
     return (
-      <fieldset className="relative w-full flex gap-3 px-3 py-2 bg-white rounded-xl border-4 border-sky-700" >
+      <fieldset className="relative w-full flex gap-3 px-3 py-2 bg-white rounded-xl border-2 border-sky-800" >
         <Search className=" text-neutral-500" size={28} />
         <input
           className=" h-full w-full text-lg bg-transparent placeholder:text-neutral-400"

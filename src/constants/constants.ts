d6a90f07fef1154a4ab4ab67647cbd7bd0a486c6
devenzonascimento <dev.enzody@gmail.com>
2024-07-19 15:18:00 -0300

    feat: rework styles and funcionalitys of  RoutineMealFormCard componet

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -10,6 +10,48 @@ export const UNIT_OPTIONS = [
   { text: "Mililitros", value: "MILILITERS" },
 ]
 
+export const HOURS_OPTIONS = [
+  { text: '00', value: '00' },
+  { text: '01', value: '01' },
+  { text: '02', value: '02' },
+  { text: '03', value: '03' },
+  { text: '04', value: '04' },
+  { text: '05', value: '05' },
+  { text: '06', value: '06' },
+  { text: '07', value: '07' },
+  { text: '08', value: '08' },
+  { text: '09', value: '09' },
+  { text: '10', value: '10' },
+  { text: '11', value: '11' },
+  { text: '12', value: '12' },
+  { text: '13', value: '13' },
+  { text: '14', value: '14' },
+  { text: '15', value: '15' },
+  { text: '16', value: '16' },
+  { text: '17', value: '17' },
+  { text: '18', value: '18' },
+  { text: '19', value: '19' },
+  { text: '20', value: '20' },
+  { text: '21', value: '21' },
+  { text: '22', value: '22' },
+  { text: '23', value: '23' },
+];
+
+export const MINUTES_OPTIONS = [
+  { text: '00', value: '00' },
+  { text: '05', value: '05' },
+  { text: '10', value: '10' },
+  { text: '15', value: '15' },
+  { text: '20', value: '20' },
+  { text: '25', value: '25' },
+  { text: '30', value: '30' },
+  { text: '35', value: '35' },
+  { text: '40', value: '40' },
+  { text: '45', value: '45' },
+  { text: '50', value: '50' },
+  { text: '55', value: '55' },
+];
+
 export const EVERY_DAY_CYCLE: CycleIllustrationType[] = [
   {
     day: 'seg',
--- src/pages/Routines/_components/routine-meal-form-card.tsx
+++ src/pages/Routines/_components/routine-meal-form-card.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react"
+import { TimeSchema, useTimeValidation } from "@/hooks/use-time-validation";
 import { useRoutineContext } from "@/context/routine-context"
 
 import { ModalBackdrop } from "@/components/modal-backdrop";
-import { TimeInput } from "@/components/time-input"
+import { SelectInput } from "@/components/select-input";
 import { Button } from "@/components/ui/button";
 
 import { Meal } from "@/types/types";
+
+import { HOURS_OPTIONS, MINUTES_OPTIONS } from "@/constants/constants";
 
 interface RoutineMealFormCardProps {
   meal: Meal;
@@ -13,34 +15,48 @@ interface RoutineMealFormCardProps {
 }
 
 export const RoutineMealFormCard = ({ meal, onClose }: RoutineMealFormCardProps) => {
-
-  const [time, setTime] = useState<string>("")
-
   const { addMeal } = useRoutineContext()
 
-  const handleAddMeal = () => {
-    addMeal({
-      meal,
-      time: time,
-    })
+  const onSubmit = (data: TimeSchema) => {
+    const time = `${data.hour}:${data.minute}`
+
+    addMeal({ meal, time })
 
     onClose()
   }
 
+  const { register, handleSubmit } = useTimeValidation()
+
   return (
     <ModalBackdrop onClose={onClose}>
-      <div className="relative w-full h-fit mx-6 flex flex-col justify-between items-center gap-6 p-4 bg-slate-100 border-4 border-sky-500 rounded-lg">
-        <h2 className="text-xl font-semibold text-center">
-          Qual horário você vai comer {meal.name}?
-        </h2>
-        <TimeInput setTime={setTime} />
-        <Button
-          onClick={handleAddMeal}
-          className="w-full bg-sky-700 hover:bg-sky-500"
-        >
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative w-full h-fit mx-6 flex flex-col justify-between items-center gap-6 p-4 bg-slate-100 border-4 border-sky-500 rounded-lg"
+      >
+        <header className="w-full text-lg font-semibold text-center text-sky-950">
+          <h1>
+            Defina o horário da refeição
+          </h1>
+          <p>
+            ({meal.name})
+          </p>
+        </header>
+        <fieldset className="w-full flex items-center justify-between gap-4">
+          <SelectInput
+            label="Horas"
+            {...register("hour")}
+            options={HOURS_OPTIONS}
+          />
+          <SelectInput
+            label="Minutos"
+            {...register("minute")}
+            options={MINUTES_OPTIONS}
+          />
+        </fieldset>
+        <Button className="w-full bg-sky-700 hover:bg-sky-500">
           Adicionar refeição
         </Button>
-      </div>
+      </form>
     </ModalBackdrop>
   );
 };

e2251726f233752b617568648d21731f49829487
devenzonascimento <dev.enzody@gmail.com>
2024-07-19 18:25:23 -0300

    fix: fix a problem with render keys

--- src/pages/Routines/RoutineDetails/components/routine-meal-item.tsx
+++ src/pages/Routines/RoutineDetails/components/routine-meal-item.tsx
@@ -9,32 +9,36 @@ interface RoutineMealItemProps {
 }
 
 export const RoutineMealItem = ({ routineMeal }: RoutineMealItemProps) => {
-  
-  const { isOpen, toggleModal } = useModalState()
-  
-  return (
-    <details className="flex flex-col gap-2 bg-white border border-sky-800 rounded-lg overflow-hidden">      
-        <summary onClick={toggleModal} className="w-full flex items-center gap-1 px-1 py-1 bg-sky-800">
-          <ChevronButton isOpen={isOpen} className="text-white"/>
-          <h1 className="text-lg font-semibold text-white truncate">
-            {routineMeal.meal.name}
-          </h1>
-          <span className="ml-auto text-lg font-semibold text-white">
-            {routineMeal.time}
-          </span>
-        </summary>
-        <main className="w-full flex flex-col items-center gap-2  p-2">
-          <section className="w-full flex flex-col items-center gap-2">
-            <h2 className="text-xl font-semibold text-sky-950">
-              Alimentos
-            </h2>
-            <List
-              data={routineMeal.meal.foods}
-              renderItem={({ item }) => <MealFoodItem key={item.id} mealFood={item} />}
-            />
-          </section>
 
-        </main>      
+  const { isOpen, toggleModal } = useModalState()
+
+  return (
+    <details className="flex flex-col gap-2 bg-white border border-sky-800 rounded-lg overflow-hidden">
+      <summary onClick={toggleModal} className="w-full flex items-center gap-1 px-1 py-1 bg-sky-800">
+        <ChevronButton isOpen={isOpen} className="text-white" />
+        <h1 className="text-lg font-semibold text-white truncate">
+          {routineMeal.meal.name}
+        </h1>
+        <span className="ml-auto text-lg font-semibold text-white">
+          {routineMeal.time}
+        </span>
+      </summary>
+      <main className="w-full flex flex-col items-center gap-2  p-2">
+        <section className="w-full flex flex-col items-center gap-2">
+          <h2 className="text-xl font-semibold text-sky-950">
+            Alimentos
+          </h2>
+          <List
+            data={routineMeal.meal.foods}
+            renderItem={({ item }) => {
+              return <MealFoodItem
+                key={routineMeal.meal.id + item.food.id}
+                mealFood={item}
+              />
+            }}
+          />
+        </section>
+      </main>
     </details>
   )
 }

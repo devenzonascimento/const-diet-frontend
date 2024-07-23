2037c5f99edb9e6dbb6f0c6ac3f7c478217ed9f2
devenzonascimento <dev.enzody@gmail.com>
2024-07-23 18:27:04 -0300

    fix: fix problem in generateDates function

--- src/functions/format-plan-data.ts
+++ src/functions/format-plan-data.ts
@@ -7,25 +7,33 @@ interface PlanData {
 }
 
 export function formatPlanData(planData: PlanData) {
+  const { cycleRoutineIds, ...plan } = planData
+
   const dates = generateDates(planData.startDate, planData.endDate);
-  const routines = assignRoutinesToDates(dates, planData.cycleRoutineIds);
+
+  const routines = assignRoutinesToDates(dates, cycleRoutineIds);
 
   return {
-    ...planData,    
+    ...plan,    
     routines: routines
   };
 }
 
 function generateDates(startDate: Date, endDate: Date): Date[] {
   const dates: Date[] = [];
+
   const currentDate = new Date(startDate);  
   currentDate.setHours(0, 0, 0, 0);
+  
+  const finalDate = new Date(endDate);  
+  finalDate.setHours(0, 0, 0, 0);
 
-  while (currentDate <= endDate) {
+  while (currentDate <= finalDate) {    
     dates.push(new Date(currentDate));
+
     currentDate.setDate(currentDate.getDate() + 1);
   }
-
+  
   return dates;
 }
 
@@ -34,8 +42,9 @@ function assignRoutinesToDates(dates: Date[], cycleRoutineIds: string[]) {
 
   for (let i = 0; i < dates.length; i++) {
     const routineId = cycleRoutineIds[i % cycleRoutineIds.length];
+    
     assignments.push({ routineId, date: dates[i] });
   }
-
+  
   return assignments;
 }

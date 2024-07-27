e587563c9f7fa748bec02ce689a0257bb8bce97d
devenzonascimento <dev.enzody@gmail.com>
2024-07-27 01:16:47 -0300

    style: increse elements sizes

--- src/components/dropdown-menu.tsx
+++ src/components/dropdown-menu.tsx
@@ -30,7 +30,7 @@ export const DropdownMenu = ({
         <button
           className="absolute top-1/2 right-0 -translate-y-1/2 h-full aspect-square flex items-center justify-center"
         >
-          <EllipsisVerticalIcon />
+          <EllipsisVerticalIcon size={32}/>
         </button>
       </PopoverTrigger>
       <DeletionConfirmation
@@ -44,15 +44,15 @@ export const DropdownMenu = ({
       />
       <PopoverContent className="w-min p-0 mr-1 border-sky-950">
         <button onClick={onEditOptionClick} className="flex items-center gap-2 p-2 text-sky-950">
-          <PencilIcon size={18} />
-          <span className="text font-medium">
+          <PencilIcon size={24} />
+          <span className="text-xl font-medium">
             Editar
           </span>
         </button>
         <hr className="border-slate-300" />
         <button onClick={toggleModal} className="flex items-center gap-2 p-2 text-red-500">
-          <Trash2Icon size={18} />
-          <span className="text font-medium">
+          <Trash2Icon size={24} />
+          <span className="text-xl font-medium">
             Excluir
           </span>
         </button>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,25 +33,25 @@ export const Header = ({
 }: HeaderProps & LeftButtonProps & RightButtonProps) => {
 
   return (
-    <header className="relative w-full flex justify-center items-center py-3 text-white bg-sky-800">
+    <header className="relative h-16 w-full flex justify-center items-center py-3 text-white bg-sky-800">
       {leftButtonNavigateTo &&
         (
           <Link
             to={leftButtonNavigateTo}
             className="absolute top-1/2 left-0 -translate-y-1/2 h-full aspect-square flex items-center justify-center"
           >
-            <ArrowLeftIcon size={28} />
+            <ArrowLeftIcon size={36} />
           </Link>
         )
       }
-      <h1 className="text-xl font-semibold">{title}</h1>
+      <h1 className="text-2xl font-semibold">{title}</h1>
       {(rightButtonNavigateTo && !rightButtonOptions) &&
         (
           <Link
             to={rightButtonNavigateTo}
             className="absolute top-1/2 right-0 -translate-y-1/2 h-full aspect-square flex items-center justify-center"
           >
-            <PlusCircleIcon size={30} />
+            <PlusCircleIcon size={34} />
           </Link>
         )
       }

bf6148364c7ea00a70d2bb95da8dc3c5a7bb82f6
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-12-14 16:24:10 -0300

    Add food services and types

--- src/services/http/food/food-service.ts
+++ src/services/http/food/food-service.ts
@@ -1,20 +1,34 @@
 import { http } from '@/services/http'
-import { Food } from '@/types/types'
+import { Food } from '@/types/food-types'
 
 const API_PREFIX = '/foods'
 
-type AddFoodRequest = Food
+export type IAddFoodService = (food: Food) => Promise<Food>
 
-type AddFoodResponse = Food
-
-export type IAddFoodService = (food: AddFoodRequest) => Promise<AddFoodResponse>
-
-export const addFoodService: IAddFoodService = async (food: AddFoodRequest) => {
-  const { data } = await http.post<AddFoodRequest, AddFoodResponse>(
-    `${API_PREFIX}`,
-    food,
-    {},
-  )
+export const addFoodService: IAddFoodService = async (food: Food) => {
+  const { data } = await http.post<Food, Food>(API_PREFIX, food)
 
   return data
+}
+
+export type IGetFoodListService = () => Promise<Food[]>
+
+export const getFoodListService: IGetFoodListService = async () => {
+  const { data } = await http.get<Food[]>(API_PREFIX)
+
+  return data
+}
+
+export type IGetFoodByIdService = (foodId: number) => Promise<Food>
+
+export const getFoodByIdService: IGetFoodByIdService = async (foodId) => {
+  const { data } = await http.get<Food>(`${API_PREFIX}/${foodId}`)
+
+  return data
+}
+
+export type IDeleteFoodService = (foodId: number) => Promise<void>
+
+export const deleteFoodService: IDeleteFoodService = async foodId => {
+  await http.delete(`${API_PREFIX}/${foodId}`)
 }

e586b23263ddacebe241aab81ff365dcdd0cc067
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-12-16 21:49:17 -0300

    Adicionado titulos para a pagina de Edição de Alimentos

--- src/pages/Foods/EditFood/edit-food.test.tsx
+++ src/pages/Foods/EditFood/edit-food.test.tsx
@@ -2,24 +2,87 @@ import React from 'react'
 import { describe, expect, it, vi } from 'vitest'
 import { fireEvent, waitFor } from '@testing-library/react'
 import { renderView } from '@/tests/render-view'
-import { useAddFoodModel } from './add-food-model'
-import { AddFoodView } from './add-food-view'
-import { IAddFoodService } from '@/services/http/food/food-service'
+import { useEditFoodModel } from './edit-food-model'
+import { EditFoodView } from './edit-food-view'
+import {
+  IGetFoodByIdService,
+  IUpdateFoodService,
+} from '@/services/http/food/food-service'
+import { Food, UnitTypes } from '@/types/food-types'
+import { delay } from '@/tests/delay'
 
-const mockAddFoodService = vi.fn<IAddFoodService>(async food => food)
-
-function MakeSut() {
-  const props = useAddFoodModel({ addFoodService: mockAddFoodService })
-  return <AddFoodView {...props} />
+const fakeFood: Food = {
+  id: 1,
+  name: 'Banana',
+  unit: UnitTypes.Grams,
+  calories: 100,
+  carbohydrates: 50,
+  proteins: 25,
+  fats: 10,
+  fibers: 5,
+  sodium: 1,
 }
 
-describe('<AddFoodPage />', () => {
+const mockGetFoodByIdService = vi.fn<IGetFoodByIdService>(async () => fakeFood)
+
+const mockUpdateFoodService = vi.fn<IUpdateFoodService>(async () => fakeFood)
+
+function MakeSut() {
+  const props = useEditFoodModel({
+    getFoodByIdService: mockGetFoodByIdService,
+    updateFoodService: mockUpdateFoodService,
+  })
+
+  return <EditFoodView {...props} />
+}
+
+describe('<EditFoodPage />', () => {
   beforeEach(() => {
-    mockAddFoodService.mockClear()
+    mockUpdateFoodService.mockClear()
+  })
+
+  it('should load the data of food to be edited correctly', async () => {
+    const screen = renderView(
+      <MakeSut />,
+      '/editar-alimento/:foodId',
+      `/editar-alimento/${fakeFood.id}`,
+    )
+
+    await delay(50)
+
+    expect(screen.getByTestId('name-input')).toHaveValue(fakeFood.name)
+    expect(screen.getByTestId('quantity-input')).toHaveValue('100')
+    expect(screen.getByTestId('calories-input')).toHaveValue(
+      fakeFood.calories.toString(),
+    )
+    expect(screen.getByTestId('carbohydrates-input')).toHaveValue(
+      fakeFood.carbohydrates.toString(),
+    )
+    expect(screen.getByTestId('proteins-input')).toHaveValue(
+      fakeFood.proteins.toString(),
+    )
+    expect(screen.getByTestId('fats-input')).toHaveValue(
+      fakeFood.fats.toString(),
+    )
+    expect(screen.getByTestId('fibers-input')).toHaveValue(
+      fakeFood.fibers.toString(),
+    )
+    expect(screen.getByTestId('sodium-input')).toHaveValue(
+      fakeFood.sodium.toString(),
+    )
+    expect(screen.getByTestId('unit-select-input')).toHaveValue(
+      fakeFood.unit.toString(),
+    )
   })
 
   it('should call service with correct params when form is submitted with valid data', async () => {
-    const screen = renderView(<MakeSut />)
+    const screen = renderView(
+      <MakeSut />,
+      '/editar-alimento/:foodId',
+      `/editar-alimento/${fakeFood.id}`,
+    )
+
+    await delay(50)
 
     const nameInput = screen.getByTestId('name-input')
     const quantityInput = screen.getByTestId('quantity-input')
@@ -50,8 +113,8 @@ describe('<AddFoodPage />', () => {
     const errorMessages = screen.queryAllByTestId('error-message')
     expect(errorMessages).toHaveLength(0)
 
-    expect(mockAddFoodService).toHaveBeenCalledWith({
-      id: 0,
+    expect(mockUpdateFoodService).toHaveBeenCalledWith({
+      id: fakeFood.id,
       name: 'Mamão',
       unit: 'GRAMS',
       calories: 100,
@@ -66,6 +129,11 @@ describe('<AddFoodPage />', () => {
   it('should show an error message if form submission fails', async () => {
     const screen = renderView(<MakeSut />)
 
+    await delay(1)
+
+    const nameInput = screen.getByTestId('name-input')
+    fireEvent.input(nameInput, { target: { value: '' } })
+
     const submitButton = screen.getByRole('button')
     fireEvent.click(submitButton)
 
@@ -77,7 +145,13 @@ describe('<AddFoodPage />', () => {
   })
 
   it('should call the service only once per submission', async () => {
-    const screen = renderView(<MakeSut />)
+    const screen = renderView(
+      <MakeSut />,
+      '/editar-alimento/:foodId',
+      `/editar-alimento/${fakeFood.id}`,
+    )
+
+    await delay(50)
 
     const nameInput = screen.getByTestId('name-input')
     fireEvent.input(nameInput, { target: { value: 'Mamão' } })
@@ -88,6 +162,6 @@ describe('<AddFoodPage />', () => {
     const formSubmit = screen.getByTestId('food-form')
     await waitFor(() => formSubmit)
 
-    expect(mockAddFoodService).toHaveBeenCalledTimes(1)
+    expect(mockUpdateFoodService).toHaveBeenCalledTimes(1)
   })
 })

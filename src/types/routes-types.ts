cecfa40bfa1b4c33ece719deeca820b200ceb47f
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-12-27 21:23:54 -0300

    feat: adicionar enumeração RouteTypes para gerenciamento de rotas

---
+++ src/types/routes-types.ts
@@ -0,0 +1,10 @@
+export enum RouteTypes {
+  MyFoodsPage = '/meus-alimentos',
+  FoodDetailsPage = '/detalhes-do-alimento',
+  CreateFoodPage = '/novo-alimento',
+  UpdateFoodPage = '/editar-alimento',
+
+  MyMealsPage = '/minhas-refeicoes',
+  CreateMealPage = '/nova-refeicao',
+  UpdateMealPage = '/editar-refeicao',
+}

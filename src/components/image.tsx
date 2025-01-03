032a5847a252db586568dde07cc599b0225146f4
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-01-03 01:39:14 -0300

    feat: adicionar suporte a variantes de tamanho no componente Image

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -2,11 +2,30 @@ import React, { useState } from 'react'
 import { Skeleton } from './ui/skeleton'
 import { cn } from '@/lib/utils'
 
+import { cva, VariantProps } from 'class-variance-authority'
+
+const imageVariants = cva(
+  'aspect-square rounded-full overflow-hidden shrink-0',
+  {
+    variants: {
+      size: {
+        small: 'w-8 h-8 text-base',
+        medium: 'w-14 h-14 text-xl',
+        large: 'w-32 h-32 text-5xl',
+        auto: 'w-auto h-auto text-xl',
+      },
+    },
+    defaultVariants: {
+      size: 'auto',
+    },
+  },
+)
+
 type ImageProps = {
   src: string
   alt: string
   className?: string
-}
+} & VariantProps<typeof imageVariants>
 
 const getInitials = (name: string) => {
   const words = name.split(' ')
@@ -18,18 +37,13 @@ const getInitials = (name: string) => {
   return name.substring(0, 2).toUpperCase()
 }
 
-export function Image({ src, alt, className }: ImageProps) {
+export function Image({ src, alt, size, className }: ImageProps) {
   const [imageStatus, setImageStatus] = useState<
     'success' | 'loading' | 'error'
   >('loading')
 
   return (
-    <div
-      className={cn(
-        'aspect-square rounded-full overflow-hidden shrink-0',
-        className,
-      )}
-    >
+    <div className={cn(imageVariants({ size, className }))}>
       {imageStatus === 'loading' && (
         <Skeleton className="size-full bg-zinc-600" />
       )}
@@ -46,10 +60,8 @@ export function Image({ src, alt, className }: ImageProps) {
       )}
 
       {imageStatus === 'error' && (
-        <div className="size-full p-2 flex items-center justify-center bg-zinc-600">
-          <span className="text-white text-xl font-bold">
-            {getInitials(alt)}
-          </span>
+        <div className="size-full p-2 flex items-center justify-center text-white font-bold bg-zinc-600">
+          {getInitials(alt)}
         </div>
       )}
     </div>
